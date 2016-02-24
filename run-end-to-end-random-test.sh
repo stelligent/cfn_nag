b6174a257ce128c85d1c1e6ff3ccbfe7c4d5027d
Eric Kascic <eric.kascic@stelligent.com>
2016-02-24 18:13:41 +0000

    not sure if it is cool to have a copy of these templates around in our repo, so always pull them down instead

--- run-end-to-end-random-test.sh
+++ run-end-to-end-random-test.sh
@@ -6,6 +6,12 @@ rvm gemset use cfn_nag --create
 
 ./deploy_local.sh
 
+mkdir spec/aws_sample_templates || true
+pushd spec/aws_sample_templates
+wget https://s3-eu-west-1.amazonaws.com/cloudformation-examples-eu-west-1/AWSCloudFormation-samples.zip
+unzip AWSCloudFormation-samples.zip
+popd
+
 for template in $(ls spec/aws_sample_templates/*.template)
 do
   echo ${template}
