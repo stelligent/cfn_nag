55e8f36cb6056b72aea9569fe5e6d987df3bbfe0
Eric Kascic <eric.kascic@stelligent.com>
2016-02-26 11:58:21 +0000

    #2 add rule to look for missing or false Encrypted bit on an EBS volume

---
+++ lib/json_rules/ebs_rules.rb
@@ -0,0 +1,3 @@
+violation jq: '[.Resources|with_entries(.value.LogicalResourceId = .key)[] | select(.Type == "AWS::EC2::Volume")|'\
+              'select(.Properties.Encrypted == null or .Properties.Encrypted == false)]|map(.LogicalResourceId) ',
+          message: 'EBS volume should have server-side encryption enabled'
