a0eb5b4071edea283f0f74695f7634ceedf5c744
Eric Kascic <eric.kascic@stelligent.com>
2016-02-29 13:01:51 +0000

    #4 add a warning about using the Authentication metadata to store credentials in the template itself

---
+++ lib/json_rules/cfn_rules.rb
@@ -0,0 +1,3 @@
+warning jq: '[.Resources|with_entries(.value.LogicalResourceId = .key)[] | select(.Metadata."AWS::CloudFormation::Authentication") ]|'\
+              'map(.LogicalResourceId) ',
+        message: 'Specifying credentials in the template itself is probably not the safest thing'
