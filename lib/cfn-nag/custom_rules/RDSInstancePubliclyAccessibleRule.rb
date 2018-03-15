f0faa76cb029ec07504fb2d47075932041b91c86
Mingchen(Jason) Xu <xysr89@outlook.com>
2018-03-15 08:24:59 -0500

    Custom rule to check publicly accessible property in RDS instance (#109)
    * added custom rule to check publicly accessible property in RDS instance

---
+++ lib/cfn-nag/custom_rules/RDSInstancePubliclyAccessibleRule.rb
@@ -0,0 +1,25 @@
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class RDSInstancePubliclyAccessibleRule < BaseRule
+
+  def rule_text
+    'RDS instance should not be publicly accessible'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'F22'
+  end
+
+  def audit_impl(cfn_model)
+    violating_rdsinstances = cfn_model.resources_by_type('AWS::RDS::DBInstance').select do |instance|
+        instance.publiclyAccessible && instance.publiclyAccessible.to_s.downcase == 'true'
+    end
+
+    violating_rdsinstances.map { |instance| instance.logical_resource_id }
+  end
+end
