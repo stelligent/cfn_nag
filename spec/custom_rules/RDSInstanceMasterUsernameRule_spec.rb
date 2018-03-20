18e0b09bd85e246da2a233ff17a3a69d2ecacfe7
xysr89 <xysr89@outlook.com>
2018-03-20 01:07:21 -0500

    remove unused cmts

--- spec/custom_rules/RDSInstanceMasterUsernameRule_spec.rb
+++ spec/custom_rules/RDSInstanceMasterUsernameRule_spec.rb
@@ -35,15 +35,4 @@ describe RDSInstanceMasterUsernameRule, :rule do
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
     end
   end
-
-  # context 'testtttttttt' do
-  #   it 'returns offending logical resource id' do
-  #     cfn_model = CfnParser.new.parse read_test_template('json/rds_instance/rds_instance_without_publicly_accessible.json')
-  
-  #     actual_logical_resource_ids = RDSInstancePubliclyAccessibleRule.new.audit_impl cfn_model
-  #     expected_logical_resource_ids = %w(AmbiguousDb)
-  
-  #     expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-  #   end
-  # end
 end
