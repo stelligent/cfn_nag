e1ea75e9339476c7bf1569c78378364b55e1a348
Jeff Bachtel <jeff@stelligent.com>
2018-03-23 15:58:10 -0400

    refactor

--- lib/cfn-nag/template_discovery.rb
+++ lib/cfn-nag/template_discovery.rb
@@ -1,20 +1,20 @@
+# Container for discovering templates
 class TemplateDiscovery
+  # input_json_path can be a directory, filename, or File
   def discover_templates(input_json_path)
     if ::File.directory? input_json_path
-      templates = find_templates_in_directory(directory: input_json_path)
-    elsif ::File.file? input_json_path
-      templates = if input_json_path.is_a? File
-                    [input_json_path.path]
-                  else
-                    [input_json_path]
-                  end
-    else
-      raise "#{input_json_path} is not a proper path"
+      return find_templates_in_directory(directory: input_json_path)
     end
-    templates
+    return [render_path(input_json_path)] if ::File.file? input_json_path
+    raise "#{input_json_path} is not a proper path"
   end
 
   private
+
+  def render_path(path)
+    return path.path if path.is_a? File
+    path
+  end
 
   def find_templates_in_directory(directory:,
                                   cfn_extensions: %w[json yaml yml template])
