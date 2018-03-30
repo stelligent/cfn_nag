026e79e9fd0b28f6b3cb95b05f6cc027ea419ea3
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2018-03-29 23:23:15 -0400

    Shorten method by extracting

--- codepipeline-lambda/lib/zip_util.rb
+++ codepipeline-lambda/lib/zip_util.rb
@@ -8,18 +8,23 @@ class ZipUtil
     raise "#{zip_file_path} not found" unless File.exist? zip_file_path
 
     Zip::File.open(zip_file_path) do |zip_file|
-      entries = zip_file.glob(file_path_within_zip)
-      if entries.empty?
-        raise "#{file_path_within_zip} not found in #{zip_file.entries}"
-      end
+      entries = check_entries zip_file, file_path_within_zip
 
       entries.each do |entry|
-        file_contents << {
-          name: entry.name,
-          contents: entry.get_input_stream.read
-        }
+        file_contents << { name: entry.name,
+                           contents: entry.get_input_stream.read }
       end
     end
     file_contents
+  end
+
+  private
+
+  def check_zip_entries(zip_file, file_path_within_zip)
+    entries = zip_file.glob(file_path_within_zip)
+    if entries.empty?
+      raise "#{file_path_within_zip} not found in #{zip_file.entries}"
+    end
+    entries
   end
 end
