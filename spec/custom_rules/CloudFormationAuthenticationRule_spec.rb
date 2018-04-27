7e4abfdf6d0668698d30738cbe39aec816d0bd27
Eric Kascic <eric.kascic@stelligent.com>
2018-04-26 23:16:33 -0400

    #104 if Metadata/CloudFormation::Authentication is a roleName, it's cool.  otherwise flag it as badness

--- spec/custom_rules/CloudFormationAuthenticationRule_spec.rb
+++ spec/custom_rules/CloudFormationAuthenticationRule_spec.rb
@@ -3,12 +3,23 @@ require 'cfn-model'
 require 'cfn-nag/custom_rules/CloudFormationAuthenticationRule'
 
 describe CloudFormationAuthenticationRule do
-  context 'authentication metadata is specified' do
+  context 'sensitive authentication metadata is specified' do
     it 'returns offending logical resource id' do
       cfn_model = CfnParser.new.parse read_test_template('json/ec2_instance/cfn_authentication.json')
 
       actual_logical_resource_ids = CloudFormationAuthenticationRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[EC2I4LBA1]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'not sensitive authentication metadata is specified' do
+    it 'returns no violation' do
+      cfn_model = CfnParser.new.parse read_test_template('json/ec2_instance/cfn_insensitive_authentication.json')
+
+      actual_logical_resource_ids = CloudFormationAuthenticationRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
     end
