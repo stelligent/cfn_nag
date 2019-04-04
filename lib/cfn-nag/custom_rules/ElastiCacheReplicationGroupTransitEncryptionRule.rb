6cae02286023783b0fa7dc2da9a79fa74504f179
Jesse R. Adams <jesse.adams@stelligent.com>
2019-04-04 15:04:34 -0700

    Adding ElasticCache ReplicationGroup encryption rules
    Closes #106 #107

---
+++ lib/cfn-nag/custom_rules/ElastiCacheReplicationGroupTransitEncryptionRule.rb
@@ -0,0 +1,27 @@
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class ElastiCacheReplicationGroupTransitEncryptionRule < BaseRule
+  def rule_text
+    'ElastiCache ReplicationGroup should have encryption enabled for in transit'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'F24'
+  end
+
+  def audit_impl(cfn_model)
+    resources = cfn_model.resources_by_type('AWS::ElastiCache::ReplicationGroup')
+
+    violating_groups = resources.select do |group|
+      group.transitEncryptionEnabled.nil? ||
+        group.transitEncryptionEnabled.to_s.casecmp('false').zero?
+    end
+
+    violating_groups.map(&:logical_resource_id)
+  end
+end
