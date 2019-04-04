5272866c241f586a36be2281619d28251db78c14
Jesse Adams <jesse.adams@stelligent.com>
2019-04-04 15:18:57 -0700

    Adding ElasticCache ReplicationGroup encryption rules (#156)
    * Adding ElasticCache ReplicationGroup encryption rules
    
    Closes #106 #107
    
    * Add boolean false tests

---
+++ lib/cfn-nag/custom_rules/ElastiCacheReplicationGroupAtRestEncryptionRule.rb
@@ -0,0 +1,27 @@
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class ElastiCacheReplicationGroupAtRestEncryptionRule < BaseRule
+  def rule_text
+    'ElastiCache ReplicationGroup should have encryption enabled for at rest'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'F25'
+  end
+
+  def audit_impl(cfn_model)
+    resources = cfn_model.resources_by_type('AWS::ElastiCache::ReplicationGroup')
+
+    violating_groups = resources.select do |group|
+      group.atRestEncryptionEnabled.nil? ||
+        group.atRestEncryptionEnabled.to_s.casecmp('false').zero?
+    end
+
+    violating_groups.map(&:logical_resource_id)
+  end
+end
---
+++ spec/custom_rules/ElastiCacheReplicationGroupTransitEncryptionRule_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require 'cfn-model'
+require 'cfn-nag/custom_rules/ElastiCacheReplicationGroupTransitEncryptionRule'
+
+describe ElastiCacheReplicationGroupTransitEncryptionRule do
+  context 'Replication Group without encryption' do
+    it 'returns offending logical resource id' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/elasticache/replication_group_with_no_encryption.json'
+      )
+
+      actual_logical_resource_ids = ElastiCacheReplicationGroupTransitEncryptionRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[BasicReplicationGroup]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Replication Group with encryption' do
+    it 'returns empty list' do
+      cfn_model = CfnParser.new.parse read_test_template('json/elasticache/replication_group_with_encryption.json')
+
+      actual_logical_resource_ids = ElastiCacheReplicationGroupTransitEncryptionRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = []
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Replication Group with encryption set to false' do
+    it 'returns offending logical resource id' do
+      cfn_model = CfnParser.new.parse read_test_template('json/elasticache/replication_group_with_encryption_false.json')
+
+      actual_logical_resource_ids = ElastiCacheReplicationGroupTransitEncryptionRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[BasicReplicationGroup]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Replication Group with encryption set to false boolean' do
+    it 'returns offending logical resource id' do
+      cfn_model = CfnParser.new.parse read_test_template('json/elasticache/replication_group_with_encryption_false_boolean.json')
+
+      actual_logical_resource_ids = ElastiCacheReplicationGroupTransitEncryptionRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[BasicReplicationGroup]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+end
