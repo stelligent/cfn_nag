6b6f2659ed8b5b391b820aef824635a160d3096a
Jesse R. Adams <jesse.adams@stelligent.com>
2019-04-09 08:46:06 -0700

    Adding custom rule for redshift cluster encryption
    Closes #51

---
+++ lib/cfn-nag/custom_rules/RedshiftClusterEncryptedRule.rb
@@ -0,0 +1,27 @@
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class RedshiftClusterEncryptedRule < BaseRule
+  def rule_text
+    'EFS FileSystem should have encryption enabled'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'F28'
+  end
+
+  def audit_impl(cfn_model)
+    resources = cfn_model.resources_by_type('AWS::Redshift::Cluster')
+
+    violating_clusters = resources.select do |cluster|
+      cluster.encrypted.nil? ||
+        cluster.encrypted.to_s.casecmp('false').zero?
+    end
+
+    violating_clusters.map(&:logical_resource_id)
+  end
+end
