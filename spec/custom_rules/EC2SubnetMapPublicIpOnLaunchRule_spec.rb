a77e4c13b5fa1c143cd1a7a522488d9ccad4d8f6
Peter Helewski <peter.helewski@stelligent.com>
2019-05-08 10:28:50 -0400

    #62 adding tests for missing and false

--- spec/custom_rules/EC2SubnetMapPublicIpOnLaunchRule_spec.rb
+++ spec/custom_rules/EC2SubnetMapPublicIpOnLaunchRule_spec.rb
@@ -3,6 +3,21 @@ require 'cfn-model'
 require 'cfn-nag/custom_rules/EC2SubnetMapPublicIpOnLaunchRule'
 
 describe EC2SubnetMapPublicIpOnLaunchRule do
+  context 'when EC2::Subnet does not have MapPublicIpOnLaunch specified' do
+    it 'returns offending logical resource id for offending Subnet' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/ec2_subnet/ec2_subnet_no_map_public_ip_on_launch_specified.yml'
+      )
+
+      actual_logical_resource_ids =
+        EC2SubnetMapPublicIpOnLaunchRule.new.audit_impl cfn_model
+
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
   context 'when EC2::Subnet has MapPublicIpOnLaunch==true' do
     it 'returns offending logical resource id for offending Subnet' do
       cfn_model = CfnParser.new.parse read_test_template(
@@ -28,6 +43,36 @@ describe EC2SubnetMapPublicIpOnLaunchRule do
         EC2SubnetMapPublicIpOnLaunchRule.new.audit_impl cfn_model
 
       expected_logical_resource_ids = %w[EC2Subnet]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'when EC2::Subnet has MapPublicIpOnLaunch==false boolean' do
+    it 'returns offending logical resource id for offending Subnet' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/ec2_subnet/ec2_subnet_map_public_ip_on_launch_false_boolean.yml'
+      )
+
+      actual_logical_resource_ids =
+        EC2SubnetMapPublicIpOnLaunchRule.new.audit_impl cfn_model
+
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'when EC2::Subnet has MapPublicIpOnLaunch==false string' do
+    it 'returns offending logical resource id for offending Subnet' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/ec2_subnet/ec2_subnet_map_public_ip_on_launch_false_string.yml'
+      )
+
+      actual_logical_resource_ids =
+        EC2SubnetMapPublicIpOnLaunchRule.new.audit_impl cfn_model
+
+      expected_logical_resource_ids = %w[]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
     end
