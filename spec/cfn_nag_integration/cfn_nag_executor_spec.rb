f972590ef6fe02aa079d99a07f29645d81bce72e
Byron Lagrone <byron.lagrone@gmail.com>
2019-05-13 13:23:31 -0500

    Test Coverage Bump
    Adds additional tests to get cfn_nag_executor and other introduced files to 100% coverage

--- spec/cfn_nag_integration/cfn_nag_executor_spec.rb
+++ spec/cfn_nag_integration/cfn_nag_executor_spec.rb
@@ -3,18 +3,23 @@ require 'spec_helper'
 require 'cfn-nag/cfn_nag_logging'
 require 'cfn-nag/cfn_nag_executor'
 
-def cli_option_mock(input_path: nil, fail_on_warnings: false)
+def cli_option_mock(input_path: nil,
+                    fail_on_warnings: false,
+                    blacklist_definition: nil,
+                    parameter_values_path: nil,
+                    profile_path: nil,
+                    output_format: 'txt')
   {
     input_path: input_path,
     fail_on_warnings: fail_on_warnings,
     allow_suppression: true,
-    blacklist_path: nil,
+    blacklist_path: blacklist_definition,
     debug: false,
     isolate_custom_rule_exceptions: false,
-    output_format: 'txt',
-    parameter_values_path: nil,
+    output_format: output_format,
+    parameter_values_path: parameter_values_path,
     print_suppression: false,
-    profile_path: nil,
+    profile_path: profile_path,
     rule_directory: nil,
     template_pattern: '..*\.json|..*\.yaml|..*\.yml|..*\.template'
   }
@@ -43,6 +48,22 @@ describe CfnNagExecutor do
     end
   end
 
+  context 'single file cfn_nag' do
+    it 'returns a successful zero exit code' do
+      test_file = File.new('spec/test_templates/yaml/ec2_subnet/ec2_subnet_map_public_ip_on_launch_true_boolean.yml', 'r')
+      argf_mock = double('argf')
+      allow(argf_mock).to receive(:eof?).and_return(false, true)
+      allow(argf_mock).to receive(:closed?).and_return(false, true)
+      allow(argf_mock).to receive(:file).and_return(test_file)
+      allow(argf_mock).to receive(:close)
+      result = @cfn_nag_executor.scan(aggregate_output: false,
+                                      argf_supplier: argf_mock,
+                                      cli_supplier: cli_option_mock)
+      test_file.close
+      expect(result).to eq 0
+    end
+  end
+
 
   # this one triggers a trollop 'system exit' mid-flow :(
   context 'no input path specified' do
@@ -57,6 +78,34 @@ describe CfnNagExecutor do
         cli_supplier: cli_option_mock(input_path: 'spec/test_templates/json/neptune'))
 
       expect(result).to eq 3
+    end
+  end
+
+  context 'input path specified with fail on warnings' do
+    it 'records four failures' do
+      result = @cfn_nag_executor.scan(
+        cli_supplier: cli_option_mock(input_path: 'spec/test_templates/yaml/ec2_subnet',
+                                      fail_on_warnings: true))
+
+      expect(result).to eq 4
+    end
+  end
+
+  context 'invalid value provided for output type' do
+    it 'dies at validate_opts' do
+      expect {
+        @cfn_nag_executor.scan(cli_supplier: cli_option_mock(output_format: 'invalid'))
+      }.to raise_error(SystemExit)
+    end
+  end
+
+  context 'use profile, blacklist, and parameter path options' do
+    it 'raises a TypeError once it tries to read the invalid files' do
+      expect {
+        @cfn_nag_executor.scan(cli_supplier: cli_option_mock(blacklist_definition: 'spec/cfn_nag_integration/test_path.txt',
+                                                         parameter_values_path: 'spec/cfn_nag_integration/test_path.txt',
+                                                         profile_path: 'spec/cfn_nag_integration/test_path.txt'))
+      }.to raise_error(TypeError)
     end
   end
 end
