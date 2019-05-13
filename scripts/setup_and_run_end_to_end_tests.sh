e972f7d08cc1559a502966b2dbbcfdbaba5e5ee4
Sean McKinley <sean.mckinley@stelligent.com>
2019-05-13 11:33:45 -0600

    [Finishes #165899494] This commit adds testing to check if cfn_nag_scan throws an exception while scanning a directory of AWS templates.

--- scripts/setup_and_run_end_to_end_tests.sh
+++ scripts/setup_and_run_end_to_end_tests.sh
@@ -1,5 +1,37 @@
 #!/bin/bash -l
 
+# Function for downloading/scanning templates to check for exceptions
+download_and_scan_templates () {
+  mkdir spec/aws_sample_templates || true
+  pushd spec/aws_sample_templates
+  curl -O https://s3-eu-west-1.amazonaws.com/cloudformation-examples-eu-west-1/AWSCloudFormation-samples.zip
+  rm *.template
+  rm -rf aws-cloudformation-templates
+  unzip AWSCloudFormation-samples.zip
+  git clone https://github.com/awslabs/aws-cloudformation-templates.git
+  popd
+  # Macros/SAM cause exceptions in cfn-model, removing these two directories
+  # allow us to successfully lint everything else.
+  rm -rf spec/aws_sample_templates/aws-cloudformation-templates/aws/services/CloudFormation/MacrosExamples
+  rm -rf spec/aws_sample_templates/aws-cloudformation-templates/community/services/Lambda/SAM
+  echo -e "\n"
+
+  # Run cfn_nag_scan on a directory of templates, scan stderr for an uppercase
+  # 'Error' which incidates an exception was thrown. For whatever reason,
+  # not every ruby exception/stacktrace contains the word 'exception'.
+  echo -e "Linting sample templates..\n"
+  cfn_nag_scan -i ./spec/aws_sample_templates 2>&1 >/dev/null | grep -A 25 Error
+
+  # Since grep exits with a status code of 0 when it maches a pattern, if
+  # the above command exits 0 then that means cfn_nag_scan threw an exception
+  if [ $? -eq 0 ]; then
+    echo -e "\nException found in cfn_nag_scan, exiting..\n"
+    exit 1
+  else
+    echo -e "\ncfn_nag_scan did not throw any exceptions, yay!"
+  fi
+}
+
 # Test for RVM and create a new gemset for local testing,
 # irrelevant for CircleCI testing, this is just to squelch errors in the
 # CircleCI build
@@ -20,3 +52,6 @@ gem install simplecov -v '~> 0.11' --no-document
 
 # Execute end-to-end tests
 rspec -t end_to_end
+
+# Call function to download & scan templates, exit 1 if an exception is thrown
+download_and_scan_templates
