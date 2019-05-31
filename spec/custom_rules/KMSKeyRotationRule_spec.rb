0d350d9f7e20cd234105809d0c77c34927880fc2
Sean McKinley <sean.mckinley@stelligent.com>
2019-05-31 13:37:03 -0600

    This commit addresses an issue where key rotation may be set to false but as a string instead of a boolean, now it will be caught and flag a warning where previously it would not have.

--- spec/custom_rules/KMSKeyRotationRule_spec.rb
+++ spec/custom_rules/KMSKeyRotationRule_spec.rb
@@ -12,6 +12,14 @@ describe KMSKeyRotationRule do
         expect(actual_logical_resource_ids).to eq []
       end
     end
+    context 'when key rotation is explicitly set to true as a string' do
+      it 'does not return an offending logical resource id' do
+        cfn_model = CfnParser.new.parse read_test_template('json/kms/kms_key_with_enable_key_rotation_true_string.json')
+        actual_logical_resource_ids = KMSKeyRotationRule.new.audit_impl cfn_model
+
+        expect(actual_logical_resource_ids).to eq []
+      end
+    end
     context 'when key rotation is explicitly set to false' do
       it 'returns an offending logical resource id' do
         cfn_model = CfnParser.new.parse read_test_template('json/kms/kms_key_with_enable_key_rotation_false.json')
@@ -20,7 +28,14 @@ describe KMSKeyRotationRule do
         expect(actual_logical_resource_ids).to eq ['myKeyRotationExplicitlyFalse']
       end
     end
+    context 'when key rotation is explicitly set to false as a string' do
+      it 'returns an offending logical resource id' do
+        cfn_model = CfnParser.new.parse read_test_template('json/kms/kms_key_with_enable_key_rotation_false_string.json')
+        actual_logical_resource_ids = KMSKeyRotationRule.new.audit_impl cfn_model
 
+        expect(actual_logical_resource_ids).to eq ['myKeyRotationExplicitlyFalseString']
+      end
+    end
     context 'when key rotation is absent' do
       it 'returns an offending logical resource id' do
         cfn_model = CfnParser.new.parse read_test_template('json/kms/kms_key_with_enable_key_rotation_absent.json')
