b9b0a3f70569ee06557d823a4d1ea241bb6a46b7
Jesse R. Adams <jesse.adams@stelligent.com>
2019-06-13 07:24:04 -0700

    Refactoring code

--- lib/cfn-nag/custom_rules/RDSDBClusterMasterUserPasswordRule.rb
+++ lib/cfn-nag/custom_rules/RDSDBClusterMasterUserPasswordRule.rb
@@ -27,29 +27,7 @@ class RDSDBClusterMasterUserPasswordRule < BaseRule
         false
       elsif cluster.masterUserPassword.is_a?(Hash) &&
             cluster.masterUserPassword.key?('Fn::If')
-        conditions = cfn_model.raw_model['Conditions']
-        condition = cluster.masterUserPassword['Fn::If'][0]
-        ref =
-          if conditions[condition].class == Array
-            conditions[condition].first['Ref']
-          else
-            conditions[condition]['Fn::Equals'].first['Ref']
-          end
-        parameter_value =
-          cfn_model.parameters[ref].synthesized_value ||=
-            cfn_model.parameters[ref].default
-        password_true = cluster.masterUserPassword['Fn::If'][1]
-        password_false = cluster.masterUserPassword['Fn::If'][2]
-
-        if truthy?(parameter_value)
-          (!cluster.snapshotIdentifier.nil? &&
-            !no_value?(password_true)) &&
-            password_is_invalid?(cfn_model, password_true)
-        else
-          (!cluster.snapshotIdentifier.nil? &&
-            !no_value?(password_false)) &&
-            password_is_invalid?(cfn_model, password_false)
-        end
+        conditional_is_invalid?(cfn_model, cluster)
       else
         password_is_invalid?(cfn_model, cluster.masterUserPassword)
       end
@@ -60,12 +38,46 @@ class RDSDBClusterMasterUserPasswordRule < BaseRule
 
   private
 
+  def determine_reference_name(cfn_model, cluster)
+    conditions = cfn_model.raw_model['Conditions']
+    condition = cluster.masterUserPassword['Fn::If'][0]
+
+    if conditions[condition].class == Array
+      conditions[condition].first['Ref']
+    else
+      conditions[condition]['Fn::Equals'].first['Ref']
+    end
+  end
+
+  def parse_conditional_values(cfn_model, cluster)
+    ref = determine_reference_name(cfn_model, cluster)
+    parameter_value =
+      cfn_model.parameters[ref].synthesized_value ||=
+        cfn_model.parameters[ref].default
+    password_true = cluster.masterUserPassword['Fn::If'][1]
+    password_false = cluster.masterUserPassword['Fn::If'][2]
+
+    [password_true, password_false, parameter_value]
+  end
+
+  def conditional_is_invalid?(cfn_model, cluster)
+    password_true, password_false, parameter_value = parse_conditional_values(cfn_model, cluster)
+
+    if truthy?(parameter_value)
+      conditional_password_is_invalid?(cfn_model, password_true, cluster.snapshotIdentifier)
+    else
+      conditional_password_is_invalid?(cfn_model, password_false, cluster.snapshotIdentifier)
+    end
+  end
+
   def password_is_invalid?(cfn_model, password)
     !no_echo_parameter_without_default?(cfn_model, password) &&
       !secrets_manager_property_value?(cfn_model, password)
   end
 
-  def no_value?(password)
-    password == { 'Ref' => 'AWS::NoValue' }
+  def conditional_password_is_invalid?(cfn_model, password, snapshot_identifier)
+    (!snapshot_identifier.nil? &&
+      password != { 'Ref' => 'AWS::NoValue' }) &&
+      password_is_invalid?(cfn_model, password)
   end
 end
