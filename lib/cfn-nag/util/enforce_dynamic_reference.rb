8df9250e0c3d07faaa9bf18b7d2b27c74b2105b7
Peter Helewski <peter.helewski@stelligent.com>
2019-06-18 08:32:25 -0400

    updating ssm comment name

--- lib/cfn-nag/util/enforce_dynamic_reference.rb
+++ lib/cfn-nag/util/enforce_dynamic_reference.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Returns true if the provided key_to_check is a dynamic reference
-# to SSM* or Secrets Manager; false otherwise.
+# to SSM Secure or Secrets Manager; false otherwise.
 def dynamic_reference_property_value?(_cfn_model, key_to_check)
   return false unless key_to_check.is_a? String
   return false unless key_to_check.start_with?('{{resolve:secretsmanager:',
