f0fe991ea7527b7f2943811aebda5b157d7883df
Peter Helewski <peter.helewski@stelligent.com>
2019-06-22 00:30:53 -0400

    adding dynamic reference tests and cleanup (#237)

--- spec/custom_rules/DirectoryServiceSimpleADPasswordRule_spec.rb
+++ spec/custom_rules/DirectoryServiceSimpleADPasswordRule_spec.rb
@@ -5,10 +5,13 @@ require 'cfn-nag/custom_rules/DirectoryServiceSimpleADPasswordRule'
 describe DirectoryServiceSimpleADPasswordRule, :rule do
 
   context 'Good SimpleAD with parameter, no echo true, and no default' do
-    it 'returns offending logical resource id' do
-      cfn_model = CfnParser.new.parse read_test_template('json/simple_ad/good_no_echo_no_default.json')
+    it 'returns empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/good_no_echo_no_default.json'
+      )
 
-      actual_logical_resource_ids = DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
@@ -17,9 +20,12 @@ describe DirectoryServiceSimpleADPasswordRule, :rule do
 
   context 'Bad SimpleAD with default password' do
     it 'returns offending logical resource id' do
-      cfn_model = CfnParser.new.parse read_test_template('json/simple_ad/with_default_password.json')
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/with_default_password.json'
+      )
 
-      actual_logical_resource_ids = DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[SimpleAD]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
@@ -28,9 +34,12 @@ describe DirectoryServiceSimpleADPasswordRule, :rule do
 
   context 'Bad SimpleAD with noecho parameter but specifies password' do
     it 'returns offending logical resource id' do
-      cfn_model = CfnParser.new.parse read_test_template('json/simple_ad/with_default_password_and_no_echo.json')
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/with_default_password_and_no_echo.json'
+      )
 
-      actual_logical_resource_ids = DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[SimpleAD]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
@@ -39,9 +48,12 @@ describe DirectoryServiceSimpleADPasswordRule, :rule do
 
   context 'Bad SimpleAD with parameter noecho set to false' do
     it 'returns offending logical resource id' do
-      cfn_model = CfnParser.new.parse read_test_template('json/simple_ad/with_no_echo_false.json')
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/with_no_echo_false.json'
+      )
 
-      actual_logical_resource_ids = DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[SimpleAD]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
@@ -50,9 +62,51 @@ describe DirectoryServiceSimpleADPasswordRule, :rule do
 
   context 'Bad SimpleAD without password parameter' do
     it 'returns offending logical resource id' do
-      cfn_model = CfnParser.new.parse read_test_template('json/simple_ad/without_parameter.json')
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/without_parameter.json'
+      )
 
-      actual_logical_resource_ids = DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[SimpleAD]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Good SimpleAD with password from Secrets Manager' do
+    it 'returns empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/simple_ad_password_secrets_manager.json'
+      )
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Good SimpleAD with password from Secure Systems Manager' do
+    it 'returns empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/simple_ad_password_ssm-secure.json'
+      )
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Good SimpleAD with password from Systems Manager' do
+    it 'returns offending logical resource id' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'json/simple_ad/simple_ad_password_ssm.json'
+      )
+      actual_logical_resource_ids =
+        DirectoryServiceSimpleADPasswordRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[SimpleAD]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
