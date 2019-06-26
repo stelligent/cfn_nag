3931621e59cfbd3786d9d728c2ddce24b46c7e69
Todd Wells <todd.wells@stelligent.com>
2019-06-25 18:29:32 -0700

    cfn templates created, first rules created

---
+++ lib/cfn-nag/custom_rules/IamRolePassRoleWildcardResourceRule.rb
@@ -0,0 +1,29 @@
+j# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class IamRolePassRoleWildcardResourceRule < BaseRule
+  def rule_text
+    'IAM role should not allow * resource on its permissions policy'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'WXX'
+  end
+
+  def audit_impl(cfn_model)
+    violating_roles = cfn_model.resources_by_type('AWS::IAM::Role').select do |role|
+      violating_policies = role.policy_objects.select do |policy|
+        !policy.policy_document.wildcard_allowed_resources.empty?
+      end
+      !violating_policies.empty?
+    end
+
+    violating_roles.map(&:logical_resource_id)
+  end
+end
