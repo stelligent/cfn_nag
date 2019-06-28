3e37333305c774bc7cb3a000dfa70ee461255cf3
Peter Helewski <peter.helewski@stelligent.com>
2019-06-28 14:05:53 -0400

    #239 adding specs for new password spec helper methods

---
+++ spec/password_rule_spec_helper_spec.rb
@@ -0,0 +1,104 @@
+require 'spec_helper'
+require 'password_rule_spec_helper'
+
+describe 'password_rule_spec_helper' do
+  before(:all) do
+    @resource_type = 'AWS::Foo::Bar'
+    @password_property = 'Baz'
+    @test_template_type = 'yaml'
+  end
+
+  context 'file_path_prefix' do
+    it 'renders path prefix' do
+      actual_aggregate_results =
+        file_path_prefix(@resource_type, @test_template_type)
+      expect(actual_aggregate_results).to eq 'yaml/foo_bar/foo_bar_'
+    end
+  end
+
+  context 'password_rule_test_sets' do
+    it 'is a hash' do
+      actual_aggregate_results = password_rule_test_sets.is_a? Hash
+      expect(actual_aggregate_results).to eq true
+    end
+  end
+
+  context 'password_rule_test_sets' do
+    it 'is a not a string' do
+      actual_aggregate_results = password_rule_test_sets.is_a? String
+      expect(actual_aggregate_results).to eq false
+    end
+  end
+
+  context 'password_rule_test_sets' do
+    it 'is a not an array' do
+      actual_aggregate_results = password_rule_test_sets.is_a? Array
+      expect(actual_aggregate_results).to eq false
+    end
+  end
+
+  context 'context_return_value as "fail"' do
+    it 'returns offending logical resource id' do
+      actual_aggregate_results = context_return_value('fail')
+      expect(actual_aggregate_results).to eq \
+        'returns offending logical resource id'
+    end
+  end
+
+  context 'context_return_value as "pass"' do
+    it 'returns empty list' do
+      actual_aggregate_results = context_return_value('pass')
+      expect(actual_aggregate_results).to eq 'returns empty list'
+    end
+  end
+
+  # context 'context_return_value as "foo"' do
+  #   it 'raises error' do
+  #     actual_aggregate_results = context_return_value('foo')
+  #     expect(actual_aggregate_results).to raise_error
+  #   end
+  # end
+
+  context 'rule_name' do
+    it 'renders rule name' do
+      actual_aggregate_results = rule_name(@resource_type, @password_property)
+      expect(actual_aggregate_results).to eq 'FooBarBazRule'
+    end
+  end
+
+  context 'file_path' do
+    it 'renders full test template file path' do
+      actual_aggregate_results =
+        file_path(@resource_type,
+                  @test_template_type,
+                  @password_property,
+                  'test template name')
+      expect(actual_aggregate_results).to eq \
+        'yaml/foo_bar/foo_bar_baz_test_template_name.yaml'
+    end
+  end
+
+  context 'expected_logical_resource_ids as "pass"' do
+    it 'returns empty list' do
+      actual_aggregate_results =
+        expected_logical_resource_ids('pass', @resource_type)
+      expect(actual_aggregate_results).to eq []
+    end
+  end
+
+  context 'expected_logical_resource_ids as "fail"' do
+    it 'returns offending resource id' do
+      actual_aggregate_results =
+        expected_logical_resource_ids('fail', @resource_type)
+      expect(actual_aggregate_results).to eq ['FooBar']
+    end
+  end
+
+  # context 'expected_logical_resource_ids as "foo"' do
+  #   it 'raises error' do
+  #     actual_aggregate_results =
+  #       expected_logical_resource_ids('foo', @resource_type)
+  #     expect(actual_aggregate_results).to raise_error
+  #   end
+  # end
+end
