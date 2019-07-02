5863ba280c90cb1215960a39fca681feca812f2e
Peter Helewski <peter.helewski@stelligent.com>
2019-07-01 20:43:17 -0400

    Create consolidated Password rule (#239)
    * create base rule to handle password checking
    
    * consolidate duplicated code for method
    
    * adjusting existing password rules to use new base password rule
    
    * #239 Creating new spec to handle password rules dynamically
    
    * #239 adding more methods and documentation
    
    * #239 adjust dynamic spec context to work with dictionary hash
    
    * 239 adding comments and cleanup old code
    
    * Refactoring password rule tests
    
    * More refactoring
    
    * #239 use new password spec helper and adjust test template file naming
    
    * #239 cleaning up code and adding descriptions to methods
    
    * #239 adding specs for new password spec helper methods
    
    * #239 fix raise tests

--- lib/cfn-nag/custom_rules/DirectoryServiceSimpleADPasswordRule.rb
+++ lib/cfn-nag/custom_rules/DirectoryServiceSimpleADPasswordRule.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require 'cfn-nag/violation'
-require 'cfn-nag/util/enforce_reference_parameter'
-require 'cfn-nag/util/enforce_string_or_dynamic_reference'
-require_relative 'base'
+require_relative 'password_base_rule'
 
 # Rule class to fail on DirectoryService::SimpleAD password in template
-class DirectoryServiceSimpleADPasswordRule < BaseRule
+class DirectoryServiceSimpleADPasswordRule < PasswordBaseRule
   def rule_text
-    'DirectoryService::SimpleAD should use a parameter for password, with NoEcho'
+    'DirectoryService SimpleAD password must not be a plaintext string ' \
+    'or a Ref to a NoEcho Parameter with a Default value.'
   end
 
   def rule_type
@@ -19,16 +18,11 @@ class DirectoryServiceSimpleADPasswordRule < BaseRule
     'F31'
   end
 
-  def audit_impl(cfn_model)
-    violating_ad = cfn_model.resources_by_type('AWS::DirectoryService::SimpleAD')
-                            .select do |ad|
-      if ad.password.nil?
-        false
-      else
-        insecure_parameter?(cfn_model, ad.password) ||
-          insecure_string_or_dynamic_reference?(cfn_model, ad.password)
-      end
-    end
-    violating_ad.map(&:logical_resource_id)
+  def resource_type
+    'AWS::DirectoryService::SimpleAD'
+  end
+
+  def password_property
+    :password
   end
 end
---
+++ spec/custom_rules/password_base_rule_spec.rb
@@ -0,0 +1,175 @@
+require 'spec_helper'
+require 'cfn-model'
+require 'cfn-nag/custom_rules/password_base_rule'
+
+describe PasswordBaseRule do
+  describe '#audit' do
+    before(:all) do
+      @base_rule = PasswordBaseRule.new
+      @base_rule.instance_eval do
+        def rule_id
+          'F3333'
+        end
+
+        def rule_type
+          Violation::FAILING_VIOLATION
+        end
+
+        def rule_text
+          'This is an epic fail!'
+        end
+
+        def resource_type
+          'AWS::Redshift::Cluster'
+        end
+
+        def password_property
+          :masterUserPassword
+        end
+      end
+    end
+
+    before(:all) do
+      @failing_violation = Violation.new(id: 'F3333',
+                                         type: Violation::FAILING_VIOLATION,
+                                         message: 'This is an epic fail!',
+                                         logical_resource_ids: %w[RedshiftCluster])
+    end
+
+    it 'raises an error when properties are not set' do
+      expect do
+        PasswordBaseRule.new.audit_impl nil
+      end.to raise_error 'must implement in subclass'
+    end
+
+    it 'returns no violation when password is not set' do
+      base_rule = @base_rule
+
+      expect(base_rule).to \
+        receive(:password_property).and_return(:masterUserPassword)
+      expect(base_rule).to \
+        receive(:resource_type).and_return('AWS::Redshift::Cluster')
+
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/redshift_cluster/' \
+        'redshift_cluster_master_user_password_not_set.yaml'
+      )
+
+      expect(base_rule.audit(cfn_model)).to be nil
+    end
+
+    it 'returns no violation when password is parameter with noecho' do
+      base_rule = @base_rule
+
+      expect(base_rule).to \
+        receive(:password_property).and_return(:masterUserPassword,
+                                               :masterUserPassword,
+                                               :masterUserPassword)
+      expect(base_rule).to \
+        receive(:resource_type).and_return('AWS::Redshift::Cluster')
+
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/redshift_cluster/' \
+        'redshift_cluster_master_user_password_parameter_with_noecho.yaml'
+      )
+
+      expect(base_rule.audit(cfn_model)).to be nil
+    end
+
+    it 'returns violation when password is literal in plaintext' do
+      base_rule = @base_rule
+
+      expect(base_rule).to \
+        receive(:password_property).and_return(:masterUserPassword,
+                                               :masterUserPassword,
+                                               :masterUserPassword)
+      expect(base_rule).to \
+        receive(:resource_type).and_return('AWS::Redshift::Cluster')
+
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/redshift_cluster/' \
+        'redshift_cluster_master_user_password_as_a_literal_in_plaintext.yaml'
+      )
+
+      expected_violation = @failing_violation
+
+      expect(base_rule.audit(cfn_model)).to eq expected_violation
+    end
+
+    it 'returns violation when password is parameter with noecho and ' \
+      'has a default value present' do
+      base_rule = @base_rule
+
+      expect(base_rule).to \
+        receive(:password_property).and_return(:masterUserPassword,
+                                               :masterUserPassword)
+      expect(base_rule).to \
+        receive(:resource_type).and_return('AWS::Redshift::Cluster')
+
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/redshift_cluster/' \
+        'redshift_cluster_master_user_password_parameter_with_noecho_and_default_value.yaml'
+      )
+
+      expected_violation = @failing_violation
+
+      expect(base_rule.audit(cfn_model)).to eq expected_violation
+    end
+
+    it 'returns no violation when password is from secrets manager' do
+      base_rule = @base_rule
+
+      expect(base_rule).to \
+        receive(:password_property).and_return(:masterUserPassword,
+                                               :masterUserPassword,
+                                               :masterUserPassword)
+      expect(base_rule).to \
+        receive(:resource_type).and_return('AWS::Redshift::Cluster')
+
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/redshift_cluster/' \
+        'redshift_cluster_master_user_password_from_secrets_manager.yaml'
+      )
+
+      expect(base_rule.audit(cfn_model)).to be nil
+    end
+
+    it 'returns no violation when password is from secure systems manager' do
+      base_rule = @base_rule
+
+      expect(base_rule).to \
+        receive(:password_property).and_return(:masterUserPassword,
+                                               :masterUserPassword,
+                                               :masterUserPassword)
+      expect(base_rule).to \
+        receive(:resource_type).and_return('AWS::Redshift::Cluster')
+
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/redshift_cluster/' \
+        'redshift_cluster_master_user_password_from_secure_systems_manager.yaml'
+      )
+
+      expect(base_rule.audit(cfn_model)).to be nil
+    end
+
+    it 'returns violation when password is systems manager' do
+      base_rule = @base_rule
+
+      expect(base_rule).to \
+        receive(:password_property).and_return(:masterUserPassword,
+                                               :masterUserPassword,
+                                               :masterUserPassword)
+      expect(base_rule).to \
+        receive(:resource_type).and_return('AWS::Redshift::Cluster')
+
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/redshift_cluster/' \
+        'redshift_cluster_master_user_password_from_systems_manager.yaml'
+      )
+
+      expected_violation = @failing_violation
+
+      expect(base_rule.audit(cfn_model)).to eq expected_violation
+    end
+  end
+end
