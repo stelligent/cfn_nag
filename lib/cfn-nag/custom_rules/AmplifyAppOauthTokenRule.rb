a62f05425ffacad6412a9ade38c60b7765433905
Peter Helewski <peter.helewski@stelligent.com>
2019-07-02 12:24:38 -0400

    #253 adding rule for Amplify App OauthToken

---
+++ lib/cfn-nag/custom_rules/AmplifyAppOauthTokenRule.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'password_base_rule'
+
+# Rule class to fail on DirectoryService::MicrosoftAD password in template
+class AmplifyAppOauthTokenRule < PasswordBaseRule
+  def rule_text
+    'Amplify App OauthToken must not be a plaintext string ' \
+    'or a Ref to a NoEcho Parameter with a Default value.'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'F38'
+  end
+
+  def resource_type
+    'AWS::Amplify::App'
+  end
+
+  def password_property
+    :oauthToken
+  end
+end
