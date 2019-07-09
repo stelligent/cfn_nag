b1d6801714062b3aba5f1630b5b00b4fdde059a4
Todd Wells <todd.wells@stelligent.com>
2019-07-09 11:31:06 -0700

    more output in che ck_pr_name.sh

--- scripts/check_pr_name.sh
+++ scripts/check_pr_name.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PR_URL=$(echo $CIRCLE_PULL_REQUEST | sed -e 's!github.com!api.github.com/repos!' -e 's!pull!pulls!' )
+curl -s -H "Authorization: token $GITHUB_API_TOKEN"  $PR_URL 
 PR_TITLE=$(curl -s -H "Authorization: token $GITHUB_API_TOKEN"  $PR_URL | jq -r '.title' )
 
 if echo $PR_TITLE | grep -v -q -E  '^#[0-9]+' 
