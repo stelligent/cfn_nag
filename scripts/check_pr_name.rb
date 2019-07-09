fcc823b625e8ef1a3eec53fabeb351a9bd56d9d9
Todd Wells <todd.wells@stelligent.com>
2019-07-09 10:04:56 -0700

    #191 fix linting issues

--- scripts/check_pr_name.rb
+++ scripts/check_pr_name.rb
@@ -5,14 +5,16 @@ require 'httparty'
 # ENV['CIRCLE_PULL_REQUEST'] = 'https://github.com/stelligent/cfn_nag/pull/243'
 # GITHUB_API_URL = 'https://api.github.com'
 
-def get_pr_title
-  *beginning, owner, repo, _pull, pr_number = ENV['CIRCLE_PULL_REQUEST'].split('/')
-  pr_url = "#{GITHUB_API_URL}/repos/#{owner}/#{repo}/pulls/#{pr_number}"
+def github_url
+  *_beginning, owner, repo, _pull, pr_number = ENV['CIRCLE_PULL_REQUEST'].split('/')
+  "#{GITHUB_API_URL}/repos/#{owner}/#{repo}/pulls/#{pr_number}"
+
+def github_pr_title
   headers = { 'Authorization' => "token #{ENV['GITHUB_TOKEN']}", 'User-Agent' => 'Httparty' }
 
   begin
-    response = HTTParty.get(pr_url, headers: headers)
-  rescue Exception => error
+    response = HTTParty.get(github_url, headers: headers)
+  rescue StandardError => error
     abort "ERROR: Exception accessing github API: #{error.message}"
   end
 
@@ -23,7 +25,7 @@ def get_pr_title
   end
 end
 
-pr_title = get_pr_title
-unless pr_title.match(/#[0-9]+/)
-  abort("ERROR: PR Title '#{pr_title}' needs to start with a issue number (example: #123 )")
+pr_title = github_pr_title
+unless title.match(/#[0-9]+/)
+  abort("ERROR: PR Title '#{title}' needs to start with a issue number (example: #123 )")
 end
