4b5f39ac9591df2211e60db48b712ff0480f98c7
Jesse R. Adams <jesse.adams@stelligent.com>
2019-07-10 09:50:25 -0700

    Fixing die message

--- lib/cfn-nag/cfn_nag_executor.rb
+++ lib/cfn-nag/cfn_nag_executor.rb
@@ -71,7 +71,7 @@ class CfnNagExecutor
   def validate_options(opts)
     unless opts[:output_format].nil? || %w[colortxt txt json].include?(opts[:output_format])
       Trollop.die(:output_format,
-                  'Must be txt or json')
+                  'Must be colortxt, txt, or json')
     end
   end
 
