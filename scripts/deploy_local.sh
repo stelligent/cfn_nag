9a3d235be2247f821eada6a07a431a92d2101963
KLEstes <klestes@gmail.com>
2019-07-19 17:57:15 -0500

    'gem build' command fixed.

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -2,5 +2,6 @@
 
 echo "Installing cfn_nag from local source"
 gem uninstall cfn-nag -x
-GEM_VERSION=0.0.01 gem build cfn-nag.gemspec
+GEM_VERSION=0.0.01
+gem build cfn-nag.gemspec
 gem install cfn-nag-0.0.01.gem --no-document
