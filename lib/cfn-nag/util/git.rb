d244586874d4914e9de4c1a4a8e9b9bbca46fecd
build <build@build.com>
2019-07-24 15:24:59 -0700

    working git utility

--- lib/cfn-nag/util/git.rb
+++ lib/cfn-nag/util/git.rb
@@ -2,19 +2,27 @@
 
 require 'git'
 
-def repo_current_info(_location)
+def repo_current_info(location)
   git = open_repo(location)
 
   current_tag = git.describe
   current_version = current_tag.gsub(/v?([0-9.]+).*/, '\1')
   major, minor, patch = current_version.split('.')
+  new_version = (current_tag.include?('-') ? "#{major}.#{minor}.#{patch.to_i + 1}" : nil)
 
   { 'tag' => current_tag,
-    'tagged_commit' => (current_tag.include?('-') ? 1 : 0),
     'version' => current_version,
     'major' => major,
     'minor' => minor,
-    'patch' => patch }
+    'patch' => patch,
+    'new_version' => new_version }
+rescue Git::GitExecuteError
+  { 'tag' => nil,
+    'version' => nil,
+    'major' => nil,
+    'minor' => nil,
+    'patch' => nil,
+    'new_version' => '0.0.1' }
 end
 
 private
@@ -25,5 +33,9 @@ def open_repo(location)
   git.config('user.email', 'build@build.com') if git.config('user.email').empty?
   git
 rescue StandardError => git_error
-  abort("Git error for #{location}: #{git_error}")
+  raise "Git error for #{location}: #{git_error}"
 end
+
+# åputs open_repo('/tmp/dir1')
+# puts repo_current_info('/tmp/repo1')
+# puts repo_current_info('~/repos/cfn_nag')
--- spec/util/git_spec.rb
+++ spec/util/git_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper'
 require 'cfn-nag/util/git.rb'
 require 'git'
 
-describe 'open_repo', :rule do
-  context 'Location is a valid repo' do
+describe 'git', :rule do
+  context 'open_repo when location is a valid repo' do
     it 'returns a repo object' do
       Dir.mktmpdir do |dir|
         Git.init(dir)
@@ -13,10 +13,51 @@ describe 'open_repo', :rule do
       end
     end
   end
-  context 'Location is not a valid repo' do
-    it 'returns error' do
+
+  context 'open_repo when location is not a valid repo' do
+    it 'raises error' do
       Dir.mktmpdir do |dir|
-        expect(open_repo(dir).to(raise_error))
+        expect { open_repo(dir) }.to raise_error(RuntimeError)
+      end
+    end
+  end
+
+  context 'repo_current_info for a repo without tags' do
+    it 'returns new_version 0.0.1' do
+      Dir.mktmpdir do |dir|
+        Git.init(dir)
+        expect(repo_current_info(dir)).to include('tag' => nil, 'version' => nil, 'major' => nil, 'minor' => nil, 'patch' => nil, 'new_version' => '0.0.1')
+      end
+    end
+  end
+
+  context 'repo_current_info for a repo with tag on latest commit' do
+    it 'returns existing version' do
+      Dir.mktmpdir do |dir|
+        Git.init(dir)
+        git = open_repo(dir)
+        File.open("#{dir}/file1", 'w+') { |file| file.write('dummy data') }
+        git.add("#{dir}/file1")
+        git.commit('test commit')
+        git.add_tag('v1.0.1', message: 'v1.0.1')
+        expect(repo_current_info(dir)).to include('tag' => 'v1.0.1', 'version' => '1.0.1', 'major' => '1', 'minor' => '0', 'patch' => '1', 'new_version' => nil)
+      end
+    end
+  end
+
+  context 'repo_current_info for a repo with commits after last tag' do
+    it 'returns existing version' do
+      Dir.mktmpdir do |dir|
+        Git.init(dir)
+        git = open_repo(dir)
+        File.open("#{dir}/file1", 'w+') { |file| file.write('dummy data') }
+        git.add("#{dir}/file1")
+        git.commit('test commit')
+        git.add_tag('v1.0.1', message: 'v1.0.1')
+        File.open("#{dir}/file2", 'w+') { |file| file.write('dummy data') }
+        git.add("#{dir}/file2")
+        git.commit('test commit2')
+        expect(repo_current_info(dir)).to include('version' => '1.0.1', 'major' => '1', 'minor' => '0', 'patch' => '1', 'new_version' => '1.0.2')
       end
     end
   end
