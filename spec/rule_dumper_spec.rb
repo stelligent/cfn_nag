d1cd659ff8055f6ddb9a178567255d84ea009250
Todd Wells <todd.wells@mphasis.com>
2019-07-29 17:12:20 -0700

    #269 Add output-format option to cfn_nag_rules (#270)
    * #269 Add output-format option to cfn_nag_rules
    
    * Fix format string, pretty print
    
    * Add rspec test for json, csv
    
    * simplify csv emit

--- spec/rule_dumper_spec.rb
+++ spec/rule_dumper_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'cfn-nag/rule_dumper'
 
 describe CfnNagRuleDumper do
@@ -11,19 +13,70 @@ describe CfnNagRuleDumper do
     end
   end
 
-  context 'simple profile' do
+  context 'simple profile and txt output format' do
     before(:each) do
       @rule_dumper = CfnNagRuleDumper.new profile_definition: "F1\nF9\n",
-                                          rule_directory: nil
+                                          rule_directory: nil,
+                                          output_format: 'txt'
     end
 
-    it 'emits list of rules' do
+    it 'emits list of rules in text' do
       expected_output = <<OUTPUTSTRING
 WARNING VIOLATIONS:
 
 FAILING VIOLATIONS:
 F1 EBS volume should have server-side encryption enabled
 F9 S3 Bucket policy should not allow Allow+NotPrincipal
+OUTPUTSTRING
+
+      expect do
+        @rule_dumper.dump_rules
+      end.to output(expected_output).to_stdout
+    end
+  end
+
+  context 'simple profile and csv ouput format' do
+    before(:each) do
+      @rule_dumper = CfnNagRuleDumper.new profile_definition: "F1\nF9\n",
+                                          rule_directory: nil,
+                                          output_format: 'csv'
+    end
+
+    it 'emits list of rules in csv' do
+      expected_output = <<OUTPUTSTRING
+Type,ID,Message
+FAIL,F1,"EBS volume should have server-side encryption enabled"
+FAIL,F9,"S3 Bucket policy should not allow Allow+NotPrincipal"
+OUTPUTSTRING
+
+      expect do
+        @rule_dumper.dump_rules
+      end.to output(expected_output).to_stdout
+    end
+  end
+
+  context 'simple profile amd json format' do
+    before(:each) do
+      @rule_dumper = CfnNagRuleDumper.new profile_definition: "F1\nF9\n",
+                                          rule_directory: nil,
+                                          output_format: 'json'
+    end
+
+    it 'emits list of rules in json' do
+      expected_output = <<OUTPUTSTRING
+[
+  {
+    "id": "F1",
+    "type": "FAIL",
+    "message": "EBS volume should have server-side encryption enabled"
+  },
+  {
+    "id": "F9",
+    "type": "FAIL",
+    "message": "S3 Bucket policy should not allow Allow+NotPrincipal"
+  }
+]
+
 OUTPUTSTRING
 
       expect do
