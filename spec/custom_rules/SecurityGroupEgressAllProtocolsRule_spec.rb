309ee988426ac996574252a496e24adf24046ecb
KLEstes <klestes@gmail.com>
2019-08-11 16:14:57 -0500

    #273  json files renamed for clarity

--- spec/custom_rules/SecurityGroupEgressAllProtocolsRule_spec.rb
+++ spec/custom_rules/SecurityGroupEgressAllProtocolsRule_spec.rb
@@ -5,7 +5,7 @@ require 'cfn-nag/custom_rules/SecurityGroupEgressAllProtocolsRule'
 describe SecurityGroupEgressAllProtocolsRule do
   context 'security group with egress rules open to port range' do
     it 'returns offending logical resource id' do
-      cfn_model = CfnParser.new.parse read_test_template('json/security_group/dangling_egress_rule_2.json')
+      cfn_model = CfnParser.new.parse read_test_template('json/security_group/dangling_allprotocols_egress_rule.json')
 
       actual_logical_resource_ids = SecurityGroupEgressAllProtocolsRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[securityGroupEgress2]
--- spec/custom_rules/SecurityGroupIngressAllProtocolsRule_spec.rb
+++ spec/custom_rules/SecurityGroupIngressAllProtocolsRule_spec.rb
@@ -6,7 +6,7 @@ describe SecurityGroupIngressAllProtocolsRule do
 
   context 'dangling security group ingress rules open to port range' do
     it 'returns offending logical resource id' do
-      cfn_model = CfnParser.new.parse read_test_template('json/security_group/dangling_ingress_rule_2.json')
+      cfn_model = CfnParser.new.parse read_test_template('json/security_group/dangling_allprotocols_ingress_rule.json')
 
       actual_logical_resource_ids = SecurityGroupIngressAllProtocolsRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[danglingIngress]
