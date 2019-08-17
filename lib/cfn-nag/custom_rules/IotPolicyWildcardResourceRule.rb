6cd32492c1a3104a61bd1a7a5872f62c3c5baa2b
Mr-Lizard <klestes@gmail.com>
2019-08-16 22:09:29 -0500

    #57 Look for wildcards in AWS::IoT::Policy (#275)
    * 'gem build' command fixed.
    
    * First stab at issue # 57
    
    * restore deploy_local.sh
    
    * Removed calls to PolicyDocumentParser
    
    * A sacrifice to appease the savage Rubocop god
    
    * Added back call to PolicyDocumentParser.new.parse
    
    * rule types corrected to Violation::WARNING
    
    * fix blank line causing linting failure
    
    * #57  Added testing for partial wildcards
    
    * #57  Added green tests to json templates

---
+++ lib/cfn-nag/custom_rules/IotPolicyWildcardResourceRule.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'base'
+require 'json'
+
+class IotPolicyWildcardResourceRule < BaseRule
+  def rule_text
+    'IoT policy should not allow * resource'
+  end
+
+  def rule_type
+    Violation::WARNING
+  end
+
+  def rule_id
+    'W39'
+  end
+
+  def audit_impl(cfn_model)
+    violating_policies = cfn_model.resources_by_type('AWS::IoT::Policy').select do |policy|
+      policy.policy_document = PolicyDocumentParser.new.parse(policy.policyDocument)
+      !policy.policy_document.wildcard_allowed_resources.empty?
+    end
+
+    violating_policies.map(&:logical_resource_id)
+  end
+end
