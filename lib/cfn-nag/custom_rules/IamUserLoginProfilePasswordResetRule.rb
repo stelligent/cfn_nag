5c60580345b2cfa2f959f97f41351fe1744efd9b
Trey McElhattan <trey.mcelhattan@stelligent.com>
2019-10-24 14:12:40 -0400

    slight change to rule text

--- lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordResetRule.rb
+++ lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordResetRule.rb
@@ -5,7 +5,7 @@ require_relative 'base'
 
 class IamUserLoginProfilePasswordResetRule < BaseRule
 	def rule_text
-		'IAM User Login Profile should exist and have PasswordResetRequired set to true'
+		'IAM User Login Profile should exist and have PasswordResetRequired property set to true'
 	end
 
 	def rule_type
@@ -20,7 +20,7 @@ class IamUserLoginProfilePasswordResetRule < BaseRule
 		violating_iam_users = cfn_model.resources_by_type('AWS::IAM::User').select do |iam_user|
 			iam_user.loginProfile['PasswordResetRequired'].nil? || iam_user.loginProfile['PasswordResetRequired'].to_s == 'false'
 		end
-		
+
 		violating_iam_users.map(&:logical_resource_id)
 	end
 end
