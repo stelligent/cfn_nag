2f710802659424ee9e74ebb64f290489136268e0
Trey McElhattan <trey.mcelhattan@stelligent.com>
2019-10-24 14:19:39 -0400

    reformatting

--- lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordResetRule.rb
+++ lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordResetRule.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-require 'cfn-nag/violation'
-require_relative 'base'
+require "cfn-nag/violation"
+require_relative "base"
 
 class IamUserLoginProfilePasswordResetRule < BaseRule
-	def rule_text
-		'IAM User Login Profile should exist and have PasswordResetRequired property set to true'
-	end
+  def rule_text
+    "IAM User Login Profile should exist and have PasswordResetRequired property set to true"
+  end
 
-	def rule_type
-		Violation::WARNING
-	end
+  def rule_type
+    Violation::WARNING
+  end
 
-	def rule_id
-		'W50'
-	end
+  def rule_id
+    "W50"
+  end
 
-	def audit_impl(cfn_model)
-		violating_iam_users = cfn_model.resources_by_type('AWS::IAM::User').select do |iam_user|
-			iam_user.loginProfile['PasswordResetRequired'].nil? || iam_user.loginProfile['PasswordResetRequired'].to_s == 'false'
-		end
+  def audit_impl(cfn_model)
+    violating_iam_users = cfn_model.resources_by_type("AWS::IAM::User").select do |iam_user|
+      iam_user.loginProfile["PasswordResetRequired"].nil? || iam_user.loginProfile["PasswordResetRequired"].to_s == "false"
+    end
 
-		violating_iam_users.map(&:logical_resource_id)
-	end
+    violating_iam_users.map(&:logical_resource_id)
+  end
 end
