ac992a571dfc09af2de9c482fa4b2a9bc4adf323
build <build@build.com>
2019-10-26 00:29:06 -0400

    #295 fix rubocop complaints

--- lib/cfn-nag/custom_rules/SecurityGroupEgressAllProtocolsRule.rb
+++ lib/cfn-nag/custom_rules/SecurityGroupEgressAllProtocolsRule.rb
@@ -22,24 +22,26 @@ class SecurityGroupEgressAllProtocolsRule < BaseRule
   def audit_impl(cfn_model)
     violating_security_groups = cfn_model.security_groups.select do |security_group|
       violating_egresses = security_group.egresses.select do |egress|
-        if egress.ipProtocol.is_a?(Integer) || egress.ipProtocol.is_a?(String)
-          egress.ipProtocol.to_i == -1
-        else
-          false
-        end
+        violating_egress(egress)
       end
 
       !violating_egresses.empty?
     end
 
     violating_egresses = cfn_model.standalone_egress.select do |standalone_egress|
-      if standalone_egress.ipProtocol.is_a?(Integer) || standalone_egress.ipProtocol.is_a?(String)
-        standalone_egress.ipProtocol.to_i == -1
-      else
-        false
-      end
+      violating_egress(standalone_egress)
     end
 
     violating_security_groups.map(&:logical_resource_id) + violating_egresses.map(&:logical_resource_id)
+  end
+
+  private
+
+  def violating_egress(egress)
+    if egress.ipProtocol.is_a?(Integer) || egress.ipProtocol.is_a?(String)
+      egress.ipProtocol.to_i == -1
+    else
+      false
+    end
   end
 end
--- lib/cfn-nag/custom_rules/SecurityGroupIngressAllProtocolsRule.rb
+++ lib/cfn-nag/custom_rules/SecurityGroupIngressAllProtocolsRule.rb
@@ -22,24 +22,26 @@ class SecurityGroupIngressAllProtocolsRule < BaseRule
   def audit_impl(cfn_model)
     violating_security_groups = cfn_model.security_groups.select do |security_group|
       violating_ingresses = security_group.ingresses.select do |ingress|
-        if ingress.ipProtocol.is_a?(Integer) || ingress.ipProtocol.is_a?(String)
-          ingress.ipProtocol.to_i == -1
-        else
-          false
-        end
+        violating_ingress(ingress)
       end
 
       !violating_ingresses.empty?
     end
 
     violating_ingresses = cfn_model.standalone_ingress.select do |standalone_ingress|
-      if standalone_ingress.ipProtocol.is_a?(Integer) || standalone_ingress.ipProtocol.is_a?(String)
-        standalone_ingress.ipProtocol.to_i == -1
-      else
-        false
-      end
+      violating_ingress(standalone_ingress)
     end
 
     violating_security_groups.map(&:logical_resource_id) + violating_ingresses.map(&:logical_resource_id)
+  end
+
+  private
+
+  def violating_ingress(ingress)
+    if ingress.ipProtocol.is_a?(Integer) || ingress.ipProtocol.is_a?(String)
+      ingress.ipProtocol.to_i == -1
+    else
+      false
+    end
   end
 end
