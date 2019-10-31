2bceb20d19ad1b15c3860fe5655a89e7b5831ec8
James Pether Sörling <pethers@users.noreply.github.com>
2019-10-31 01:42:56 +0100

    Rule for api gateway access logging enabled https://github.com/stelligent/cfn_nag/issues/299 (#300)
    * Initial work on https://github.com/stelligent/cfn_nag/issues/299
    
    * fix test

---
+++ lib/cfn-nag/custom_rules/ApiGatewayAccessLoggingRule.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class ApiGatewayAccessLoggingRule < BaseRule
+  def rule_text
+    'ApiGateway should have access logging configured'
+  end
+
+  def rule_type
+    Violation::WARNING
+  end
+
+  def rule_id
+    'W44'
+  end
+
+  def audit_impl(cfn_model)
+    violating_deployments = cfn_model.resources_by_type('AWS::ApiGateway::Deployment').select do |deployment|
+      deployment.stageDescription.nil? || deployment.stageDescription['AccessLogSetting'].nil?
+    end
+
+    violating_deployments.map(&:logical_resource_id)
+  end
+end
---
+++ spec/custom_rules/ApiGatewayAccessLoggingRule_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'cfn-model'
+require 'cfn-nag/custom_rules/ApiGatewayAccessLoggingRule'
+
+describe ApiGatewayAccessLoggingRule do
+  context 'Api Gateway has no access logging configured since it missing stagedescription' do
+    it 'returns offending logical resource id' do
+      cfn_model = CfnParser.new.parse read_test_template('json/apigateway_accesslogging/apigateway_with_no_access_logging_missing_stagedescription.json')
+
+      actual_logical_resource_ids = ApiGatewayAccessLoggingRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[ApiGatewayNoAccessLogging]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Api Gateway has no access logging configured' do
+    it 'returns offending logical resource id' do
+      cfn_model = CfnParser.new.parse read_test_template('json/apigateway_accesslogging/apigateway_with_no_access_logging.json')
+
+      actual_logical_resource_ids = ApiGatewayAccessLoggingRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[ApiGatewayNoAccessLogging]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'Api Gateway should has access logging configured' do
+    it 'returns empty' do
+      cfn_model = CfnParser.new.parse read_test_template('json/apigateway_accesslogging/apigateway_with_access_logging.json')
+
+      actual_logical_resource_ids = ApiGatewayAccessLoggingRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+end
