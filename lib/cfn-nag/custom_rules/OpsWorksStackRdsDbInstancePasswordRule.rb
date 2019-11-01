990b82486f02b9ccd19a8d39b8d43a41b2496b60
Trey McElhattan <trey.mcelhattan@stelligent.com>
2019-11-01 11:56:48 -0400

    Refactoring to check to see if RdsDbInstances property exists and/or the DbPassword key is defined

--- lib/cfn-nag/custom_rules/OpsWorksStackRdsDbInstancePasswordRule.rb
+++ lib/cfn-nag/custom_rules/OpsWorksStackRdsDbInstancePasswordRule.rb
@@ -8,7 +8,8 @@ require_relative 'base'
 class OpsWorksStackRdsDbInstancePasswordRule < BaseRule
 
   def rule_text
-    'OpsWorks Stack RDS DBInstance Password property should not show password in plain text, resolve an unsecure ssm string, or have a default value for parameter.'
+    'OpsWorks Stack RDS DBInstance Password property should not show password ' +
+    'in plain text, resolve an unsecure ssm string, or have a default value for parameter.'
   end
 
   def rule_type
@@ -19,23 +20,46 @@ class OpsWorksStackRdsDbInstancePasswordRule < BaseRule
     'F54'
   end
 
-  def resource_type
-    'AWS::OpsWorks::Stack'
+  def check_password_property(cfn_model, dbinstance)
+    # Checks to make sure 'RdsDbInstances' property has the key 'Password' defined.
+    # Also check if the value for the 'Password' key is nil
+    if dbinstance.has_key? 'DbPassword'
+      if insecure_parameter?(cfn_model, dbinstance['DbPassword'])
+        true
+      elsif insecure_string_or_dynamic_reference?(cfn_model, dbinstance['DbPassword'])
+        true
+      elsif dbinstance['DbPassword'].nil?
+        true
+      end
+    else
+      true    
+    end
   end
+
+  def check_dbinstance_property(dbinstance_property)
+    # Checks to see if the 'RdsDbInstances' property that is part of the 'AWS::OpsWorks::Stack'
+    # resource exists and is defined.
+    !dbinstance_property.nil?
+  end
+
+  def get_violating_dbinstances(cfn_model, resource)
+    # If the 'RdsDbInstances' property is defined then grab the violating DB Instances
+    # that are part of the 'RdsDbInstances' property list.
+    if check_dbinstance_property(resource.rdsDbInstances)
+      violating_dbinstances = resource.rdsDbInstances.select do |dbinstance|
+        check_password_property(cfn_model, dbinstance)
+      end
+      !violating_dbinstances.empty?
+    else
+      false
+    end
+  end 
 
   def audit_impl(cfn_model)
     resources = cfn_model.resources_by_type('AWS::OpsWorks::Stack')
-
     violating_resources = resources.select do |resource|
-      violating_db_instances = resource.rdsDbInstances.select do |dbinstance|
-        insecure_parameter?(cfn_model, dbinstance['DbPassword']) || \
-        insecure_string_or_dynamic_reference?(cfn_model, dbinstance['DbPassword']) || \
-        dbinstance['DbPassword'].nil?
-      end
-      !violating_db_instances.empty?
+      get_violating_dbinstances(cfn_model, resource)
     end
-
     violating_resources.map(&:logical_resource_id)
   end
 end 
-
--- spec/custom_rules/OpsWorksStackRdsDbInstancePasswordRule_spec.rb
+++ spec/custom_rules/OpsWorksStackRdsDbInstancePasswordRule_spec.rb
@@ -4,8 +4,8 @@ require 'cfn-model'
 require 'cfn-nag/custom_rules/OpsWorksStackRdsDbInstancePasswordRule'
 
 describe OpsWorksStackRdsDbInstancePasswordRule do
-  context 'OpsWorks Stack RDS DBInstance resource with parameter password with NoEcho but default value' do
-    it 'Returns the logical resource ID of the offending AWS::OpsWorks::Stack resouce' do
+  context 'OpsWorks Stack RDS DBInstance resource with parameter DbPassword with NoEcho but default value' do
+    it 'Returns the logical resource ID of the offending AWS::OpsWorks::Stack resource' do
       cfn_model = CfnParser.new.parse read_test_template(
         'yaml/ops_works/ops_works_stack_rds_db_instance_password_parameter_noecho_with_default.yaml'
       )
@@ -18,8 +18,8 @@ describe OpsWorksStackRdsDbInstancePasswordRule do
     end
   end
 
-  context 'OpsWorks Stack RDS DBInstance resource with parameter password with default value' do
-    it 'Returns the logical resource ID of the offending AWS::OpsWorks::Stack resouce' do
+  context 'OpsWorks Stack RDS DBInstance resource with parameter DbPassword with default value' do
+    it 'Returns the logical resource ID of the offending AWS::OpsWorks::Stack resource' do
       cfn_model = CfnParser.new.parse read_test_template(
         'yaml/ops_works/ops_works_stack_rds_db_instance_password_parameter_default.yaml'
       )
@@ -32,7 +32,7 @@ describe OpsWorksStackRdsDbInstancePasswordRule do
     end
   end
 
-  context 'OpsWorks Stack RDS DBInstance resource with parameter password with NoEcho' do
+  context 'OpsWorks Stack RDS DBInstance resource with parameter DbPassword with NoEcho' do
     it 'returns empty list' do
       cfn_model = CfnParser.new.parse read_test_template(
         'yaml/ops_works/ops_works_stack_rds_db_instance_password_parameter_noecho.yaml'
@@ -46,8 +46,8 @@ describe OpsWorksStackRdsDbInstancePasswordRule do
     end
   end
 
-  context 'OpsWorks Stack RDS DBInstance resource has a password in plain text' do
-    it 'Returns the logical resource ID of the offending AWS::OpsWorks::Stack resouce' do
+  context 'OpsWorks Stack RDS DBInstance resource has a DbPassword in plain text' do
+    it 'Returns the logical resource ID of the offending AWS::OpsWorks::Stack resource' do
       cfn_model = CfnParser.new.parse read_test_template(
         'yaml/ops_works/ops_works_stack_rds_db_instance_password_plain_text.yaml'
       )
@@ -60,7 +60,7 @@ describe OpsWorksStackRdsDbInstancePasswordRule do
     end
   end
 
-  context 'OpsWorks Stack RDS DBInstance has password from Secrets Manager' do
+  context 'OpsWorks Stack RDS DBInstance has DbPassword from Secrets Manager' do
     it 'returns empty list' do
       cfn_model = CfnParser.new.parse read_test_template(
         'yaml/ops_works/ops_works_stack_rds_db_instance_password_secrets_manager.yaml'
@@ -73,7 +73,7 @@ describe OpsWorksStackRdsDbInstancePasswordRule do
     end
   end
 
-  context 'OpsWorks Stack RDS DBInstance has password from Secure String in Systems Manager' do
+  context 'OpsWorks Stack RDS DBInstance has DbPassword from Secure String in Systems Manager' do
     it 'returns empty list' do
       cfn_model = CfnParser.new.parse read_test_template(
         'yaml/ops_works/ops_works_stack_rds_db_instance_password_ssm_secure.yaml'
@@ -86,14 +86,53 @@ describe OpsWorksStackRdsDbInstancePasswordRule do
     end
   end
 
-  context 'OpsWorks Stack RDS DBInstance has password from Not Secure String in Systems Manager' do
-    it 'returns offending logical resource id for offending AWS::OpsWorks::Stack resouce' do
+  context 'OpsWorks Stack RDS DBInstance has DbPassword from Not Secure String in Systems Manager' do
+    it 'returns offending logical resource id for offending AWS::OpsWorks::Stack resource' do
       cfn_model = CfnParser.new.parse read_test_template(
         'yaml/ops_works/ops_works_stack_rds_db_instance_password_ssm_not_secure.yaml'
       )
       actual_logical_resource_ids =
         OpsWorksStackRdsDbInstancePasswordRule.new.audit_impl cfn_model
       expected_logical_resource_ids = %w[MyOpsWorksStack MyOpsWorksStack2]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'OpsWorks Stack RDS DBInstance exists but with DbPassword key not defined' do
+    it 'returns offending logical resource id for offending AWS::OpsWorks::Stack resource' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/ops_works/ops_works_stack_rds_db_instance_password_key_not_defined.yaml'
+      )
+      actual_logical_resource_ids =
+        OpsWorksStackRdsDbInstancePasswordRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[MyOpsWorksStack2]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'OpsWorks Stack RDS DBInstance exists but  DbPassword key value is nil. ' do
+    it 'returns offending logical resource id for offending AWS::OpsWorks::Stack resource' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/ops_works/ops_works_stack_rds_db_instance_password_key_value_nil.yaml'
+      )
+      actual_logical_resource_ids =
+        OpsWorksStackRdsDbInstancePasswordRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[MyOpsWorksStack2]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context 'OpsWorks Stack RDS DBInstance property does NOT exist (as it is not a required property).' do
+    it 'returns empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+        'yaml/ops_works/ops_works_stack_rds_db_instance_property_not_defined.yaml'
+      )
+      actual_logical_resource_ids =
+        OpsWorksStackRdsDbInstancePasswordRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
 
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
     end
