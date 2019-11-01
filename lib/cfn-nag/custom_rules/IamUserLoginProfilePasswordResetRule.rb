2193d12f33c05a90e86e5597ce6fc28437443852
Trey McElhattan <trey.mcelhattan@stelligent.com>
2019-11-01 17:08:25 -0400

    cleaning up

--- lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordResetRule.rb
+++ lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordResetRule.rb
@@ -48,11 +48,4 @@ class IamUserLoginProfilePasswordResetRule < BaseRule
 
     violating_iam_users.map(&:logical_resource_id)
   end
-  # def audit_impl(cfn_model)
-  #   violating_iam_users = cfn_model.resources_by_type('AWS::IAM::User').select do |iam_user|
-  #     iam_user.loginProfile['PasswordResetRequired'].nil? || iam_user.loginProfile['PasswordResetRequired'].to_s == 'false'
-  #   end
-
-  #   violating_iam_users.map(&:logical_resource_id)
-  # end
 end
