acb38c22c9d1b5df3a220e33167cc7f499491e82
Trey McElhattan <trey.mcelhattan@stelligent.com>
2019-11-06 11:25:18 -0500

    additional refactors

--- lib/cfn-nag/custom_rules/AmazonMQBrokerUserPasswordRule.rb
+++ lib/cfn-nag/custom_rules/AmazonMQBrokerUserPasswordRule.rb
@@ -20,9 +20,17 @@ class AmazonMQBrokerUserPasswordRule < BaseRule
     'F52'
   end
 
-  def check_password_property(cfn_model, user)
-    # Checks to make sure 'Users' property has the key 'Password' defined.
-    # Also check if the value for the 'Password' key is nil
+  def audit_impl(cfn_model)
+    resources = cfn_model.resources_by_type('AWS::AmazonMQ::Broker')
+    violating_resources = resources.select do |mq_broker|
+      violating_users?(cfn_model, mq_broker)
+    end
+    violating_resources.map(&:logical_resource_id)
+  end
+
+  private
+
+  def user_has_insecure_password?(cfn_model, user)
     if user.has_key? 'Password'
       if insecure_parameter?(cfn_model, user['Password'])
         true
@@ -36,32 +44,14 @@ class AmazonMQBrokerUserPasswordRule < BaseRule
     end
   end
 
-  def check_user_property(user_property)
-    # Checks to make sure that the 'Users' property that is part of the 'AWS::AmazonMQ::Broker'
-    # resource exists and is defined.
-    if !user_property.nil?
-      true
-    end
-  end
-
-  def get_violating_users(cfn_model, resource)
-    # If the 'Users' property is defined then try to grab the violating user password keys
-    # that are oare of the 'Users' property list.
-    if check_user_property(resource.users)
-      violating_users = resource.users.select do |user|
-        check_password_property(cfn_model, user)
+  def violating_users?(cfn_model, mq_broker)
+    if !mq_broker.users.nil?
+      violating_users = mq_broker.users.select do |user|
+        user_has_insecure_password?(cfn_model, user)
       end
       !violating_users.empty?
     else
       true
     end
   end 
-
-  def audit_impl(cfn_model)
-    resources = cfn_model.resources_by_type('AWS::AmazonMQ::Broker')
-    violating_resources = resources.select do |resource|
-      get_violating_users(cfn_model, resource)
-    end
-    violating_resources.map(&:logical_resource_id)
-  end
 end
