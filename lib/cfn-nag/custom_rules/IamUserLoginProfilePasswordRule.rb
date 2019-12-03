1da1ccc7529266ad7641eb29aea50b73a9ee26df
Peter Helewski <peter.helewski@stelligent.com>
2019-12-03 12:52:24 -0500

    #253 refactoring iam user loginprofile password rule to use password base rule helper

--- lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordRule.rb
+++ lib/cfn-nag/custom_rules/IamUserLoginProfilePasswordRule.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 require 'cfn-nag/violation'
-require 'cfn-nag/util/enforce_reference_parameter'
-require 'cfn-nag/util/enforce_string_or_dynamic_reference'
-require_relative 'base'
+require_relative 'password_base_rule'
 
-class IamUserLoginProfilePasswordRule < BaseRule
+class IAMUserLoginProfilePasswordRule < PasswordBaseRule
   def rule_text
-    'If the IAM user LoginProile property exists, then its Password value should not ' +
-    'show password in plain text, resolve an unsecure ssm string, or have a default value for parameter.'
+    'IAM User LoginProfile Password must not be a plaintext string or ' \
+    'a Ref to a NoEcho Parameter with a Default value.' \
   end
 
   def rule_type
@@ -19,35 +17,15 @@ class IamUserLoginProfilePasswordRule < BaseRule
     'F51'
   end
 
-  def audit_impl(cfn_model)
-    resources = cfn_model.resources_by_type('AWS::IAM::User')
-    violating_resources = resources.select do |iam_user|
-      violating_users?(cfn_model, iam_user)
-    end
-    violating_resources.map(&:logical_resource_id)
+  def resource_type
+    'AWS::IAM::User'
   end
 
-  private
-  
-  def iam_user_has_insecure_password?(cfn_model, login_profile)
-    if login_profile.has_key? 'Password'
-      if insecure_parameter?(cfn_model, login_profile['Password'])
-        true
-      elsif insecure_string_or_dynamic_reference?(cfn_model, login_profile['Password'])
-        true
-      elsif login_profile['Password'].nil?
-        true
-      end
-    else
-      true    
-    end
+  def password_property
+    :loginProfile
   end
 
-  def violating_users?(cfn_model, iam_user)
-    if !iam_user.loginProfile.nil?
-      iam_user_has_insecure_password?(cfn_model, iam_user.loginProfile)
-    else
-      false
-    end
-  end 
+  def sub_property_name
+    'Password'
+  end
 end
--- spec/custom_rules/IamUserLoginProfilePasswordRule_spec.rb
+++ spec/custom_rules/IamUserLoginProfilePasswordRule_spec.rb
@@ -1,140 +1,22 @@
 require 'spec_helper'
 require 'password_rule_spec_helper'
 require 'cfn-model'
-require 'cfn-nag/custom_rules/IamUserLoginProfilePasswordRule'
 
-describe IamUserLoginProfilePasswordRule do
-  context 'IAM User resource LoginProfile property with parameter password with NoEcho but default value' do
-    it 'Returns the logical resource ID of the offending IAMUser' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_parameter_noecho_with_default.yaml'
-      )
+resource_type = 'AWS::IAM::User'
+password_property = 'LoginProfile'
+sub_property_name = 'Password'
+test_template_type = 'yaml'
 
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[MyIamUser]
+require "cfn-nag/custom_rules/#{rule_name(resource_type, password_property, sub_property_name)}"
 
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM User resource LoginProfile property with parameter password with default value' do
-    it 'Returns the logical resource ID of the offending IAMUser' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_parameter_default.yaml'
-      )
-
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[MyIamUser]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM User resource LoginProfile property with parameter password with NoEcho' do
-    it 'returns empty list' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_parameter_noecho.yaml'
-      )
-
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM User resource LoginProfile property has a password in plain text' do
-    it 'Returns the logical resource ID of the offending IAMUser' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_plaintext.yaml'
-      )
-
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[MyIamUser]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM user resource LoginProfile property has password from Secrets Manager' do
-    it 'returns empty list' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_secrets_manager.yaml'
-      )
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM user resource LoginProfile property has password from Secure String in Systems Manager' do
-    it 'returns empty list' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_ssm_secure.yaml'
-      )
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM user resource LoginProfile property has password from Not Secure String in Systems Manager' do
-    it 'returns offending logical resource id for offending IAMUser' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_ssm_not_secure.yaml'
-      )
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[MyIamUser]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM user resource LoginProfile property is not defined' do
-    it 'empty list' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_property_not_defined.yaml'
-      )
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM user resource LoginProfile property has password value as nil' do
-    it 'returns offending logical resource id for offending IAMUser' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_key_nil.yaml'
-      )
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[MyIamUser]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
-    end
-  end
-
-  context 'IAM user resource LoginProfile property has password key not defined.' do
-    it 'returns offending logical resource id for offending IAMUser' do
-      cfn_model = CfnParser.new.parse read_test_template(
-        'yaml/iam_user/iam_user_login_profile_password_key_not_defined.yaml'
-      )
-      actual_logical_resource_ids =
-        IamUserLoginProfilePasswordRule.new.audit_impl cfn_model
-      expected_logical_resource_ids = %w[MyIamUser]
-
-      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+describe Object.const_get(rule_name(resource_type, password_property, sub_property_name)), :rule do
+  # Creates dynamic set of contexts based on the password_rule_test_sets hash
+  password_rule_test_sets.each do |test_description, desired_test_result|
+    context "#{resource_type} #{password_property} #{sub_property_name} #{test_description}" do
+      it context_return_value(desired_test_result) do
+        run_test(resource_type, password_property, sub_property_name,
+                 test_template_type, test_description, desired_test_result)
+      end
     end
   end
 end
