2af860ab76c04ca46b8c5d2835a216d4a7d6ee87
build <build@build.com>
2019-12-06 19:39:16 -0500

    #313 decompose rule to satisfy rubocop

--- lib/cfn-nag/custom_rules/ElasticLoadBalancerV2AccessLoggingRule.rb
+++ lib/cfn-nag/custom_rules/ElasticLoadBalancerV2AccessLoggingRule.rb
@@ -18,10 +18,26 @@ class ElasticLoadBalancerV2AccessLoggingRule < BaseRule
 
   def audit_impl(cfn_model)
     violating_elbs = cfn_model.resources_by_type('AWS::ElasticLoadBalancingV2::LoadBalancer')
-                              .select do |elb|      
-      elb.loadBalancerAttributes.nil? || elb.loadBalancerAttributes.find{ |k| k['Key'] ==  'access_logs.s3.enabled'}.nil? ||  elb.loadBalancerAttributes.find{ |k| k['Key'] ==  'access_logs.s3.enabled' && k['Value'].casecmp?('false') } 
+                              .select do |elb|
+      elb.loadBalancerAttributes.nil? || missing_access_logs?(elb) || access_logging_is_false?(elb)
     end
 
     violating_elbs.map(&:logical_resource_id)
+  end
+
+  private
+
+  def access_logging_is_false?(load_balancer)
+    false_access_log_attribute = load_balancer.loadBalancerAttributes.find do |load_balancer_attribute|
+      load_balancer_attribute['Key'] ==  'access_logs.s3.enabled' && load_balancer_attribute['Value'].casecmp?('false')
+    end
+    false_access_log_attribute
+  end
+
+  def missing_access_logs?(load_balancer)
+    access_log_attribute = load_balancer.loadBalancerAttributes.find do |load_balancer_attribute|
+      load_balancer_attribute['Key'] ==  'access_logs.s3.enabled'
+    end
+    access_log_attribute.nil?
   end
 end
