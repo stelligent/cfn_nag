37a53d60c46087b3576bbdf8fb9ec4e2759d8530
Peter Helewski <peter.helewski@stelligent.com>
2020-01-16 12:10:38 -0500

    #253 adding check to ensure the optional property exists in template before running secure check against value

--- lib/cfn-nag/custom_rules/ManagedBlockchainMemberMemberFabricConfigurationAdminPasswordRule.rb
+++ lib/cfn-nag/custom_rules/ManagedBlockchainMemberMemberFabricConfigurationAdminPasswordRule.rb
@@ -22,15 +22,34 @@ class ManagedBlockchainMemberMemberFabricConfigurationAdminPasswordRule < BaseRu
   def audit_impl(cfn_model)
     managed_blockchain_members = cfn_model.resources_by_type('AWS::ManagedBlockchain::Member')
     violating_managed_blockchains = managed_blockchain_members.select do |member|
-      member_config = member.memberConfiguration['MemberFrameworkConfiguration']['MemberFabricConfiguration']['AdminPassword']
-      if member_config.nil?
+      if password_property_does_not_exist(member)
         false
       else
-        insecure_parameter?(cfn_model, member_config) ||
-          insecure_string_or_dynamic_reference?(cfn_model, member_config)
+        pw = member.memberConfiguration['MemberFrameworkConfiguration']['MemberFabricConfiguration']['AdminPassword']
+        insecure_parameter?(cfn_model, pw) ||
+          insecure_string_or_dynamic_reference?(cfn_model, pw)
       end
     end
 
     violating_managed_blockchains.map(&:logical_resource_id)
+  end
+
+  private
+
+  # Checks to see if these properties are present as they are optional
+  # properties for the 'AWS::ManagedBlockchain::Member' resource:
+  #   'MemberFrameworkConfiguration'
+  #   'MemberFabricConfiguration'
+  #   'AdminPassword'
+  def password_property_does_not_exist(member)
+    if member.memberConfiguration['MemberFrameworkConfiguration'].nil?
+      true
+    elsif member.memberConfiguration['MemberFrameworkConfiguration']['MemberFabricConfiguration'].nil?
+      true
+    elsif member.memberConfiguration['MemberFrameworkConfiguration']['MemberFabricConfiguration']['AdminPassword'].nil?
+      true
+    else
+      false
+    end
   end
 end
