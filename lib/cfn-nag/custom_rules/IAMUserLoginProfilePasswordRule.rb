d6b3c7fd012706a8ff3d662bee0df1eaf6a9f147
Peter Helewski <peter.helewski@stelligent.com>
2020-01-16 11:01:09 -0500

    Feature/253 update iam user login profile password rule (#347)
    * #253 removing existing IAM User LoginProfile Password rule files to prepare for new files with uppercase naming
    
    * #253 adding IAM User LoginProfile Password rule back in with upper case IAM in rule and spec name
    
    * Update IAMUserLoginProfilePasswordRule.rb
    
    Co-authored-by: Eric Kascic <eric.kascic@stelligent.com>

---
+++ lib/cfn-nag/custom_rules/IAMUserLoginProfilePasswordRule.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'password_base_rule'
+
+class IAMUserLoginProfilePasswordRule < PasswordBaseRule
+  def rule_text
+    'IAM User LoginProfile Password must not be a plaintext string or ' \
+    'a Ref to a NoEcho Parameter with a Default value.'
+  end
+
+  def rule_type
+    Violation::FAILING_VIOLATION
+  end
+
+  def rule_id
+    'F51'
+  end
+
+  def resource_type
+    'AWS::IAM::User'
+  end
+
+  def password_property
+    :loginProfile
+  end
+
+  def sub_property_name
+    'Password'
+  end
+end
