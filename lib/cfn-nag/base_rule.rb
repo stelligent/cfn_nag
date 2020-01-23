3507dbd075e9277147b7e07f1788b1087c16c1f4
erickascic <eric.kascic@stelligent.com>
2020-01-22 23:50:08 -0500

    #222 testing intergation with SAR pipeline

---
+++ lib/cfn-nag/base_rule.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+
+# Base class all Rules should subclass
+class CfnNag
+  class BaseRule
+    ##
+    # Returns a collection of logical resource ids
+    #
+    def audit_impl(_cfn_model)
+      raise 'must implement in subclass'
+    end
+
+    ##
+    # Returns nil when there are no violations
+    # Returns a Violation object otherwise
+    #
+    def audit(cfn_model)
+      logical_resource_ids = audit_impl(cfn_model)
+      return if logical_resource_ids.empty?
+
+      Violation.new(id: rule_id,
+                    type: rule_type,
+                    message: rule_text,
+                    logical_resource_ids: logical_resource_ids)
+    end
+  end
+end
