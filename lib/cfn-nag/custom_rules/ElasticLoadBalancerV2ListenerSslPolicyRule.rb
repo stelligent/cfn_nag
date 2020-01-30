50243a629209a5c2229a7294ff3783407b171f40
Stephen Goncher <stephen.goncher@stelligent.com>
2020-01-30 10:23:31 -0500

    Add initial set of elb listener rule and spec tests for ssl policy

---
+++ lib/cfn-nag/custom_rules/ElasticLoadBalancerV2ListenerSslPolicyRule.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class ElasticLoadBalancerV2ListenerSslPolicyRule < BaseRule
+  def rule_text
+    'Elastic Load Balancer V2 Listener SslPolicy should use TLS 1.2'
+  end
+
+  def rule_type
+    Violation::WARNING
+  end
+
+  def rule_id
+    'W77'
+  end
+
+  def audit_impl(cfn_model)
+    violating_listeners = cfn_model.resources_by_type('AWS::ElasticLoadBalancingV2::Listener')
+                                   .select do |listener|
+      if listener.protocol == 'HTTPS' || listener.protocol == 'TLS'
+        listener.sslPolicy.nil? ||
+          %w[ELBSecurityPolicy-2016-08 ELBSecurityPolicy-TLS-1-0-2015-04 ELBSecurityPolicy-TLS-1-1-2017-01
+             ELBSecurityPolicy-FS-2018-06 ELBSecurityPolicy-FS-1-1-2019-08 ELBSecurityPolicy-2015]
+            .include?(listener.sslPolicy)
+      else
+        next
+      end
+    end
+
+    violating_listeners.map(&:logical_resource_id)
+  end
+end
