c33f4c585cf648897bc0dd946f0d9c9c999b48d4
Stephen Goncher <stephen.goncher@stelligent.com>
2020-01-31 16:49:04 -0500

    Fix rubocop for SslPolicyRule

--- lib/cfn-nag/custom_rules/ElasticLoadBalancerV2ListenerSslPolicyRule.rb
+++ lib/cfn-nag/custom_rules/ElasticLoadBalancerV2ListenerSslPolicyRule.rb
@@ -19,9 +19,9 @@ class ElasticLoadBalancerV2ListenerSslPolicyRule < BaseRule
   def audit_impl(cfn_model)
     violating_listeners = cfn_model.resources_by_type('AWS::ElasticLoadBalancingV2::Listener')
                                    .select do |listener|
-        %w[ELBSecurityPolicy-2016-08 ELBSecurityPolicy-TLS-1-0-2015-04 ELBSecurityPolicy-TLS-1-1-2017-01
-           ELBSecurityPolicy-FS-2018-06 ELBSecurityPolicy-FS-1-1-2019-08 ELBSecurityPolicy-2015]
-          .include?(listener.sSLPolicy)
+      %w[ELBSecurityPolicy-2016-08 ELBSecurityPolicy-TLS-1-0-2015-04 ELBSecurityPolicy-TLS-1-1-2017-01
+         ELBSecurityPolicy-FS-2018-06 ELBSecurityPolicy-FS-1-1-2019-08 ELBSecurityPolicy-2015]
+        .include?(listener.sSLPolicy)
     end
 
     violating_listeners.map(&:logical_resource_id)
