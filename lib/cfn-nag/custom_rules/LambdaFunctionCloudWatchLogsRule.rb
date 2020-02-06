63907714d650195ea7b6b5a767ffe2efee9f6bff
Patrick Shelby <patrick.shelby@stelligent.com>
2020-02-06 13:27:53 -0500

    #76 Moving much of the logic in LambdaFunctionCloudWatchLogsRule.rb to stelligent/cfn-model for reusability.

--- lib/cfn-nag/custom_rules/LambdaFunctionCloudWatchLogsRule.rb
+++ lib/cfn-nag/custom_rules/LambdaFunctionCloudWatchLogsRule.rb
@@ -1,14 +1,9 @@
 # frozen_string_literal: true
 
-require 'cfn-nag/util/wildcard_patterns'
 require 'cfn-nag/violation'
 require_relative 'base'
 
 class LambdaFunctionCloudWatchLogsRule < BaseRule
-  IAM_CREATEGROUP_PATTERNS = wildcard_patterns('CreateLogGroup').map! { |x| 'logs:' + x } + ['*']
-  IAM_CREATESTREAM_PATTERNS = wildcard_patterns('CreateLogStream').map! { |x| 'logs:' + x } + ['*']
-  IAM_PUTEVENT_PATTERNS = wildcard_patterns('PutLogEvent').map! { |x| 'logs:' + x } + ['*']
-
   def rule_text
     'Lambda functions require permission to write CloudWatch Logs'
   end
@@ -25,60 +20,45 @@ class LambdaFunctionCloudWatchLogsRule < BaseRule
     # Iterate over each Lambda function
     lambda_functions = cfn_model.resources_by_type('AWS::Lambda::Function')
     violating_lambda_functions = lambda_functions.select do |lambda_function|
-      # Determine how the role is assigned to the function
-      # and get the role ID
+      # Throw warning if role is a string ARN
       next lambda_function if lambda_function.role.instance_of? String
 
       # Find role in template if exists
-      role_resources = find_role_by_id?(cfn_model, compute_role_id?(lambda_function.role.values[0]))
+      # If ID not in template, add lambda to violations and continue
+      role_resources = cfn_model.resources_by_id(lambda_function.role_id)
+      next lambda_function if role_resources.empty?
 
       # Add lambda as violating if meets conditions
-      violating_roles = find_violating_roles?(role_resources)
-      role_resources.empty? || !violating_roles.empty?
+      role_violations = violating_roles?(role_resources)
+      !role_violations.empty?
     end
 
     violating_lambda_functions.map(&:logical_resource_id)
   end
 
-  def creategroup_action?(statement)
-    statement.actions.find { |action| IAM_CREATEGROUP_PATTERNS.include? action }
-  end
-
-  def createstream_action?(statement)
-    statement.actions.find { |action| IAM_CREATESTREAM_PATTERNS.include? action }
-  end
-
-  def putevent_action?(statement)
-    statement.actions.find { |action| IAM_PUTEVENT_PATTERNS.include? action }
-  end
-
-  def compute_role_id?(role_value)
-    role_value.instance_of?(String) ? role_value.split('.')[0] : role_value[0]
-  end
-
-  def find_role_by_id?(model, role_id)
-    model.resources.values.select { |resource| resource.logical_resource_id == role_id }
-  end
-
-  def evaluate_managed_policies?(managed_policies)
+  def managed_policies_include_cw_logs_access?(managed_policies)
     managed_policies.include?('arn:aws:iam::aws:policy/CloudWatchLogsFullAccess') || \
       managed_policies.include?('arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole')
   end
 
-  def find_violating_roles?(roles)
+  def inline_policies_include_cw_logs_access?(policies)
+    policies.select do |policy|
+      permissive_statements = policy.policy_document.statements.select do |statement|
+        statement.allows_action?('logs:CreateLogGroup') && \
+          statement.allows_action?('logs:CreateLogStream') && \
+          statement.allows_action?('logs:PutLogEvent')
+      end
+      !permissive_statements.empty?
+    end
+  end
+
+  def violating_roles?(roles)
     roles.select do |role|
       # Iterate over each policy in role
-      permissive_policies = role.policy_objects.select do |policy|
-        permissive_statements = policy.policy_document.statements.select do |statement|
-          creategroup_action?(statement) && \
-            createstream_action?(statement) && \
-            putevent_action?(statement)
-        end
-        !permissive_statements.empty?
-      end
+      permissive_policies = inline_policies_include_cw_logs_access?(role.policy_objects)
 
       # Iterate over each managed policy in role
-      permissive_managed_policies = evaluate_managed_policies?(role.managedPolicyArns)
+      permissive_managed_policies = managed_policies_include_cw_logs_access?(role.managedPolicyArns)
 
       # Check if any policies violated
       permissive_policies.empty? && !permissive_managed_policies
