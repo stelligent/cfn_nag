2c3adcaa931dc5a1858c961de9610a355b143371
Stephen Goncher <stephen.goncher@stelligent.com>
2020-02-07 09:07:23 -0500

    Fix the Listener SslPolicy parameter name casing (#363)
    * #60 Fix the SslPolicy parameter name
    
    * #60 Append extra line to listener yml spec files

--- lib/cfn-nag/custom_rules/ElasticLoadBalancerV2ListenerSslPolicyRule.rb
+++ lib/cfn-nag/custom_rules/ElasticLoadBalancerV2ListenerSslPolicyRule.rb
@@ -29,11 +29,11 @@ class ElasticLoadBalancerV2ListenerSslPolicyRule < BaseRule
 
   def violating_listeners?(listener)
     if %w[HTTPS TLS].include?(listener.protocol)
-      listener.sSLPolicy.nil? ||
+      listener.sslPolicy.nil? ||
         %w[ELBSecurityPolicy-2016-08 ELBSecurityPolicy-TLS-1-0-2015-04
            ELBSecurityPolicy-TLS-1-1-2017-01 ELBSecurityPolicy-FS-2018-06
            ELBSecurityPolicy-FS-1-1-2019-08 ELBSecurityPolicy-2015]
-          .include?(listener.sSLPolicy)
+          .include?(listener.sslPolicy)
     else
       false
     end
