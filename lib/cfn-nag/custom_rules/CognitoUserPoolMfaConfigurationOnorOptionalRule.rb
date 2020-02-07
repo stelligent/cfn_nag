84efe74f1b438dfb8ad6d2354e611b4e76d5a69e
Trey McElhattan <trey.mcelhattan@stelligent.com>
2020-02-07 16:46:18 -0500

    Initial commit for Issue #67 - Adding rule to check for Cognito UserPool MfaConfiguration set to 'ON' or 'OPTIONAL'

---
+++ lib/cfn-nag/custom_rules/CognitoUserPoolMfaConfigurationOnorOptionalRule.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require 'cfn-nag/util/enforce_reference_parameter'
+require_relative 'base'
+
+class CognitoUserPoolMfaConfigurationOnorOptionalRule < BaseRule
+  def rule_text
+    "AWS Cognito UserPool should have MfaConfiguration set to 'ON' (MUST be wrapped in quotes) or at least 'OPTIONAL'"
+  end
+
+  def rule_type
+    Violation::WARNING
+  end
+
+  def rule_id
+    'W85'
+  end
+
+  def audit_impl(cfn_model)
+    violating_userpools = cfn_model.resources_by_type('AWS::Cognito::UserPool').select do |userpool|
+      violating_userpool?(userpool)
+    end
+
+    violating_userpools.map(&:logical_resource_id)
+  end
+
+  private
+
+  def yaml_reserved_keyword_boolean_conv?(user_pool, param_value = '')
+    if [true, false].include?(user_pool.mfaConfiguration)
+      true
+    elsif [true, false].include?(param_value)
+      true
+    end
+  end
+
+  def keyword_uppercase?(user_pool)
+    violating_key_words = %w[On on OFF Off off Optional optional]
+    violating_key_words.include? user_pool.mfaConfiguration
+  end
+
+  def param_reference?(user_pool)
+    violating_key_words = %w[On on OFF Off off Optional optional]
+    ref_value = user_pool.mfaConfiguration.values[0]
+    if user_pool.cfn_model.parameters[ref_value].default.nil?
+    else
+      param_value = user_pool.cfn_model.parameters[ref_value].default
+      violating_key_words.include?(param_value) || yaml_reserved_keyword_boolean_conv?(user_pool, param_value)
+    end
+  end
+
+  def violating_userpool?(user_pool)
+    if user_pool.mfaConfiguration.class == Hash
+      param_reference?(user_pool)
+    else
+      keyword_uppercase?(user_pool) || \
+        yaml_reserved_keyword_boolean_conv?(user_pool)
+    end
+  end
+end
---
+++ spec/custom_rules/CognitoUserPoolMfaConfigurationOnorOptionalRule_spec.rb
@@ -0,0 +1,140 @@
+require 'spec_helper'
+require 'cfn-model'
+require 'cfn-nag/custom_rules/CognitoUserPoolMfaConfigurationOnorOptionalRule'
+
+describe CognitoUserPoolMfaConfigurationOnorOptionalRule do
+  context "Cognito UserPool with MfaConfiguration set to 'ON' (Wrapped in quotes, i.e 'ON')." do
+    it 'Returns an empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_on_wrapped_in_quotes.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context "Cognito UserPool with MfaConfiguration set to ON/On/on (NOT wrapped in quotes)." do
+    it 'Returns offending logical resource ids' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_on_not_wrapped_in_quotes.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[CognitoUserPool2 CognitoUserPool3 CognitoUserPool4]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+  #
+  context "Cognito UserPool with MfaConfiguration set to 'On' or 'on' (Wrapped in quotes but NOT fully upper case)." do
+    it 'Returns offending logical resource ids' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_on_wrapped_in_quotes_not_uppercase.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[CognitoUserPool1 CognitoUserPool2]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context "Cognito UserPool with MfaConfiguration set to OFF/Off/off or 'OFF'/'Off'/'off' or
+  (Wrapped/Not wrapped in quotes and/or NOT fully upper case)." do
+    it 'Returns offending logical resource ids' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_off_all_variations.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[CognitoUserPool1
+                                         CognitoUserPool2
+                                         CognitoUserPool3
+                                         CognitoUserPool4
+                                         CognitoUserPool5
+                                         CognitoUserPool6]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context "Cognito UserPool with MfaConfiguration set to 'Optional'/'optional' (NOT uppercase)." do
+    it 'Returns offending logical resource ids' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_optional_not_uppercase.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[CognitoUserPool1 CognitoUserPool2 CognitoUserPool3 CognitoUserPool4]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context "Cognito UserPool with MfaConfiguration set to OPTIONAL/'OPTIONAL' (uppercase)." do
+    it 'Returns an empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_optional_uppercase.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context "Cognito UserPool with MfaConfiguration set to OFF/Off/off or 'OFF'/'Off'/'off' or
+  when referenced by parameter values (Wrapped/Not wrapped in quotes and/or NOT fully upper case)." do
+    it 'Returns offending logical resource ids' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_violations_all_variations_with_param_refs.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[CognitoUserPool1
+                                         CognitoUserPool2
+                                         CognitoUserPool3
+                                         CognitoUserPool4
+                                         CognitoUserPool5
+                                         CognitoUserPool6
+                                         CognitoUserPool7
+                                         CognitoUserPool8
+                                         CognitoUserPool9
+                                         CognitoUserPool10
+                                         CognitoUserPool11
+                                         CognitoUserPool12
+                                         CognitoUserPool13]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+  context "Cognito UserPool with MfaConfiguration with parameters references but not default values" do
+    it 'Returns an empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_parameter_refs_no_default_values.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context "Cognito UserPool with MfaConfiguration set to OPTIONAL/'OPTIONAL' or 'ON' with parameter references (uppercase)." do
+    it 'Returns an empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_user_pool_mfa_configuration_on_optional_parameter_refs.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoUserPoolMfaConfigurationOnorOptionalRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+end
