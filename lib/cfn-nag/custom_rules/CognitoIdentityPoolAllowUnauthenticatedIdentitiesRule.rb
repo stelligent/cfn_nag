dea1e919c5588cc753737a1d14c389707621c1f9
Trey McElhattan <trey.mcelhattan@stelligent.com>
2020-02-13 15:12:03 -0500

    Refactored rule. also added a util script and a method for checking if a property is referencing a parameter and another method for getting that parameter's Default value.

--- lib/cfn-nag/custom_rules/CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule.rb
+++ lib/cfn-nag/custom_rules/CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require 'cfn-nag/violation'
-require_relative 'base'
 require 'cfn-nag/util/truthy'
+require 'cfn-nag/util/parameter_reference_and_default_value'
+require_relative 'base'
 
 class CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule < BaseRule
   def rule_text
@@ -20,7 +21,7 @@ class CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule < BaseRule
 
   def audit_impl(cfn_model)
     violating_identity_pools = cfn_model.resources_by_type('AWS::Cognito::IdentityPool').select do |identity_pool|
-      violating_identity_pool?(identity_pool.allowUnauthenticatedIdentities)
+      violating_identity_pool?(identity_pool)
     end
 
     violating_identity_pools.map(&:logical_resource_id)
@@ -28,7 +29,20 @@ class CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule < BaseRule
 
   private
 
+  def violations?(property_value)
+    truthy?(property_value)
+  end
+
   def violating_identity_pool?(identity_pool)
-    truthy?(identity_pool)
+    ip_prop = identity_pool.allowUnauthenticatedIdentities
+    if property_a_param_ref?(identity_pool, ip_prop)
+      prop_default_value = get_default_param_value(identity_pool, ip_prop)
+      if prop_default_value.nil?
+      else
+        violations?(prop_default_value)
+      end
+    else
+      violations?(ip_prop)
+    end
   end
 end
--- spec/custom_rules/CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule_spec.rb
+++ spec/custom_rules/CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule_spec.rb
@@ -28,4 +28,30 @@ describe CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule do
       expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
     end
   end
+
+  context "AWS::Cognito::IdentityPool AllowUnauthenticatedIdentities with Default value parameter references. " do
+    it 'Returns offending logical resource ids' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_identity_pool_allowunauthenticatedidentities_default_value_param_refs.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[CognitoIdentityPool1 CognitoIdentityPool2]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
+
+  context "AWS::Cognito::IdentityPool AllowUnauthenticatedIdentities with parameter references but no Default values. " do
+    it 'Returns an empty list' do
+      cfn_model = CfnParser.new.parse read_test_template(
+                                          'yaml/cognito/cognito_identity_pool_allowunauthenticatedidentities_param_refs_no_default_values.yaml'
+                                      )
+
+      actual_logical_resource_ids = CognitoIdentityPoolAllowUnauthenticatedIdentitiesRule.new.audit_impl cfn_model
+      expected_logical_resource_ids = %w[]
+
+      expect(actual_logical_resource_ids).to eq expected_logical_resource_ids
+    end
+  end
 end
