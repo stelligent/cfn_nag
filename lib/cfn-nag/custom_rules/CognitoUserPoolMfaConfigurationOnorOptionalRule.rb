7879cbb504f2f68bcaf16678ea387d1c3bdcc6c5
Trey McElhattan <trey.mcelhattan@stelligent.com>
2020-02-17 15:58:19 -0500

    rubocop

--- lib/cfn-nag/custom_rules/CognitoUserPoolMfaConfigurationOnorOptionalRule.rb
+++ lib/cfn-nag/custom_rules/CognitoUserPoolMfaConfigurationOnorOptionalRule.rb
@@ -2,7 +2,6 @@
 
 require 'cfn-nag/violation'
 require 'cfn-nag/util/truthy'
-#require 'cfn-nag/util/parameter_reference_and_default_value'
 require_relative 'base'
 
 class CognitoUserPoolMfaConfigurationOnorOptionalRule < BaseRule
