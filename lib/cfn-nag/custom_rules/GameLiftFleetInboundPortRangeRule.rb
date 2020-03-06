81f40cadc40c5bd984bb2131b5fe4580acfb5347
Patrick Shelby <patrick.shelby@stelligent.com>
2020-03-06 23:03:32 +0000

    Making S3 bucket more secure and linting.

--- lib/cfn-nag/custom_rules/GameLiftFleetInboundPortRangeRule.rb
+++ lib/cfn-nag/custom_rules/GameLiftFleetInboundPortRangeRule.rb
@@ -19,8 +19,6 @@ class GameLiftFleetInboundPortRangeRule < BaseRule
   def audit_impl(cfn_model)
     violating_gamelift_fleets = cfn_model.resources_by_type('AWS::GameLift::Fleet').select do |gamelift_fleet|
       violating_permissions = gamelift_fleet.eC2InboundPermissions.select do |permission|
-        # subnet_mask = permission['IpRange'].split('/').length > 1 ? permission['IpRange'].split('/')[1].to_i : 32
-        # permission['FromPort'] != permission['ToPort'] || subnet_mask < 24
         permission['FromPort'] != permission['ToPort']
       end
 
