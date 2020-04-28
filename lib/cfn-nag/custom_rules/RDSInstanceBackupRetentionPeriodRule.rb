c481acce3c5f2d0b22ed216f35f0e6bb780a9f43
Daniel <3842788+dscpinheiro@users.noreply.github.com>
2020-04-28 14:28:32 +0000

    #438 Add rule that checks if RDS instance has a backup retention period gr…
    * Add rule that checks if RDS instance has a backup retention period greater than 0
    
    * Update template to check for string values as well
    
    * Refactor if statement to check for nil
    
    * Remove blank lines
    
    Co-authored-by: Daniel Pinheiro <dspin@amazon.com>

---
+++ lib/cfn-nag/custom_rules/RDSInstanceBackupRetentionPeriodRule.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class RDSInstanceBackupRetentionPeriodRule < BaseRule
+  def rule_text
+    'RDS instance should have backup retention period greater than 0'
+  end
+
+  def rule_type
+    Violation::WARNING
+  end
+
+  def rule_id
+    'W75'
+  end
+
+  def audit_impl(cfn_model)
+    rds_dbinstances = cfn_model.resources_by_type('AWS::RDS::DBInstance')
+
+    violating_rdsinstances = rds_dbinstances.select do |instance|
+      violating_period(instance.backupRetentionPeriod)
+    end
+
+    violating_rdsinstances.map(&:logical_resource_id)
+  end
+
+  def violating_period(backup_retention_period)
+    if backup_retention_period.nil?
+      false
+    end
+
+    if backup_retention_period.is_a?(Integer) || backup_retention_period.is_a?(String)
+      backup_retention_period.to_i == 0
+    else
+      false
+    end
+  end
+end
