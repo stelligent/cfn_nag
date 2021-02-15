d6638ed3967c42f329fda987c20aeb395df8971d
Daniel <3842788+dscpinheiro@users.noreply.github.com>
2021-02-15 19:32:48 +0000

    Add rule that checks if SSE is enabled for Kinesis Firehose delivery stream (of type DirectPut) (#523)
    Closes https://github.com/stelligent/cfn_nag/issues/521

---
+++ lib/cfn-nag/custom_rules/KinesisFirehoseDeliveryStreamEncryptionRule.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'cfn-nag/violation'
+require_relative 'base'
+
+class KinesisFirehoseDeliveryStreamEncryptionRule < BaseRule
+  def rule_text
+    'Kinesis Firehose DeliveryStream of type DirectPut should specify SSE.'
+  end
+
+  def rule_type
+    Violation::WARNING
+  end
+
+  def rule_id
+    'W88'
+  end
+
+  def audit_impl(cfn_model)
+    violating_delivery_streams = cfn_model.resources_by_type('AWS::KinesisFirehose::DeliveryStream').select do |delivery_stream|
+      violating_delivery_stream?(delivery_stream)
+    end
+
+    violating_delivery_streams.map(&:logical_resource_id)
+  end
+
+  private
+
+  def violating_delivery_stream?(delivery_stream)
+    if delivery_stream.deliveryStreamType == 'KinesisStreamAsSource'
+      false
+    elsif delivery_stream.deliveryStreamEncryptionConfigurationInput.nil?
+      true
+    else
+      delivery_stream.deliveryStreamEncryptionConfigurationInput['KeyType'].nil?
+    end
+  end
+end
