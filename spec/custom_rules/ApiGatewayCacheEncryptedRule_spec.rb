bf12c6b40a4f5761f601aa196dc2cf3e5c4804ba
James Pether Sörling <pethers@users.noreply.github.com>
2021-02-16 20:16:40 +0100

    Update spec/custom_rules/ApiGatewayCacheEncryptedRule_spec.rb
    correct test message
    
    Co-authored-by: Kevin Formsma <kevin.formsma@gmail.com>

--- spec/custom_rules/ApiGatewayCacheEncryptedRule_spec.rb
+++ spec/custom_rules/ApiGatewayCacheEncryptedRule_spec.rb
@@ -4,7 +4,7 @@ require 'cfn-nag/custom_rules/ApiGatewayCacheEncryptedRule'
 
 describe ApiGatewayCacheEncryptedRule do
   context 'Api Gateway has cache encryption enabled' do
-    it 'returns offending logical resource id' do
+    it 'returns no violating resources' do
       cfn_model = CfnParser.new.parse read_test_template('json/apigateway_cacheencrypted/apigateway_with_cache_encryption_enabled.json')
 
       actual_logical_resource_ids = ApiGatewayCacheEncryptedRule.new.audit_impl cfn_model
